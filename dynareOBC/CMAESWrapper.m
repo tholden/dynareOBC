5b5163bd91671d5d135e282e05123e3df9f67fda
tholden <thomas.holden@gmail.com>
2015-08-28 16:22:22 -0700

    better parallelisation
    Former-commit-id: 3cefbe28fc5bf8596c7846a0e987f1d4ba5b8d3b

--- dynareOBC/CMAESWrapper.m
+++ dynareOBC/CMAESWrapper.m
@@ -1,6 +1,13 @@
 function [ x, f ] = CMAESWrapper( OptiFunction, x, lb, ub, varargin )
+    try
+        pool = parpool;
+        nw = pool.NumWorkers;
+    catch
+        nw = 1;
+    end
 	cmaesOptions = cmaes;
 	cmaesOptions.EvalParallel = 1;
+    cmaesOptions.PopSize = [ 'max( ' int2str( nw ) ', (4 + floor(3*log(N))) )' ];
 	cmaesOptions.LBounds = lb;
 	cmaesOptions.UBounds = ub;
 	cmaesOptions.CMA.active = 1;
