f76d2d32e0f947e4ad6f29564566159718460964
tholden <thomas.holden@gmail.com>
2015-10-26 23:06:47 -0700

    relax global bounds problem slightly
    Former-commit-id: 20a1458f3b60a46829ea5c1b54b861481e707e80 [formerly 4036eeb5b951b3fba4ce3ed1af20e5b416b309e7]
    Former-commit-id: 1d0dfd3d181aae260d2054f9b10a5490cabc1140

--- dynareOBC/SolveGlobalBoundsProblem.m
+++ dynareOBC/SolveGlobalBoundsProblem.m
@@ -12,11 +12,15 @@ function y = SolveGlobalBoundsProblem( y, Ey, UnconstrainedReturnPathShortRun, U
     
     yResiduals = y - Ey;
     ConstraintResiduals = sdpvar( length( y ), 1 );
+    Violations = sdpvar( length( y ), 1 );
     
-    Constraints = [ 0 == W1 .* ( DesiredReturnPath - ConstrainedReturnPathLongRun ) + W2 .* ConstraintResiduals, ConstrainedReturnPathLongRun >= 0, y + ConstrainedReturnPathLongRun - ConstrainedReturnPathShortRun >= 0 ];
-    Diagnostics = optimize( Constraints, ( ConstraintResiduals' * ConstraintResiduals ) * dynareOBC.GlobalConstraintStrength + yResiduals' * yResiduals, dynareOBC.LPOptions );
+    Constraints = [ 0 == W1 .* ( DesiredReturnPath - ConstrainedReturnPathLongRun ) + W2 .* ConstraintResiduals, ConstrainedReturnPathLongRun >= 0, y + ConstrainedReturnPathLongRun - ConstrainedReturnPathShortRun >= -Violations, Violations >= 0 ];
+    Diagnostics = optimize( Constraints, ( ConstraintResiduals' * ConstraintResiduals ) * dynareOBC.GlobalConstraintStrength + yResiduals' * yResiduals + 1e8 * ( Violations' * Violations ), dynareOBC.LPOptions );
     if Diagnostics.problem ~= 0
         error( 'dynareOBC:FailedToSolveGlobalBoundsProblem', [ 'An apparently impossible quadratic progrmaming problem was encountered when solving the global bounds problem. Internal message: ' Diagnostics.info ] );
+    end
+    if max( value( Violations ) ) > sqrt( eps )
+        warning( 'dynareOBC:InaccuracyInGlobalBoundsProblem', [ 'The solution to the global bounds problem may violate a constraint of the form max{0,x}>=x.' ] );
     end
     y = value( y );
 
