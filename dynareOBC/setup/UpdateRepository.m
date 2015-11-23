ab9a96c4866278c267f4cbddc352b692fbeb9e44
tholden <thomas.holden@gmail.com>
2015-11-23 15:13:29 -0800

    use a force checkout to copy files
    Former-commit-id: 95fa11a179cd09a2cfd697a79eb080f769cf29c9 [formerly 126b43045ea915f864c13ae3c4a861149bdaad9d]
    Former-commit-id: d0cea758fe2963e42189ffce7d74393bbc102f85

--- dynareOBC/setup/UpdateRepository.m
+++ dynareOBC/setup/UpdateRepository.m
@@ -48,7 +48,7 @@ function UpdateRepository( Directory, GitDirectory, Remote )
         if RepoError
             DestinationGitDirectory = [ tempname '/' ];
             disp( [ 'Moving old Git directory from: ' CurrentGitDirectory ' to ' DestinationGitDirectory ] );
-            movefile( CurrentGitDirectory, DestinationGitDirectory, 'f' );
+            MoveFiles( CurrentGitDirectory, DestinationGitDirectory );
             CurrentGitDirectory = [];
         end
     else
@@ -63,17 +63,32 @@ function UpdateRepository( Directory, GitDirectory, Remote )
         cloneCMD.setBare( false );
         cloneCMD.setCloneAllBranches( false );
         cloneCMD.setCloneSubmodules( true );
-        cloneCMD.setNoCheckout( false );
+        cloneCMD.setNoCheckout( true );
         cloneCMD.setProgressMonitor( com.mikofski.jgit4matlab.MATLABProgressMonitor );
         cloneCMD.setURI( Remote );
         cloneCMD.setBranch( 'master' );
         cloneCMD.call( );
         
-        MoveFiles( TemporaryLocation, Directory );
+        MoveFiles( [ TemporaryLocation '.git/' ], GitDirectory );
         
         disp( [ 'Succesfully cloned the latest files from the ' RepositoryName ' repository.' ] );
 
-        CurrentGitDirectory = LongDefaultGitDirectory;
+        CurrentGitDirectory = GitDirectory;
+        
+        RepoBuilder = org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+
+        RepoBuilder.readEnvironment;
+        RepoBuilder.setWorkTree( GetJavaFile( Directory ) );
+        RepoBuilder.setGitDir( GetJavaFile( CurrentGitDirectory ) );
+        RepoBuilder.setMustExist( true );
+
+        gitRepository = RepoBuilder.build;
+        gitAPI = org.eclipse.jgit.api.Git( gitRepository );
+        checkoutCMD = gitAPI.checkout( );
+        checkoutCMD.setAllPaths( true );
+        checkoutCMD.setForce( true );
+        checkoutCMD.setName( 'master' );
+        checkoutCMD.call( );
     end
 
     SourceDestEqual = false;
