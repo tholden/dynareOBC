165ab00a48babc500cba25b90daa1fd36c51a108
tholden <thomas.holden@gmail.com>
2015-11-23 12:31:36 -0800

    improved update code
    Former-commit-id: 37e53cc322268a0d942bb38709c87ca98126ec9b [formerly 1d2af320a2ff8335812b7855aa03b9cc4b5b1c45]
    Former-commit-id: f0dc383afce785d968bbb30a254d78e48c1f29da

--- dynareOBC/setup/UpdateRepository.m
+++ dynareOBC/setup/UpdateRepository.m
@@ -1,62 +1,94 @@
-function UpdateRepository( directory, gitDirectory, remote )
-%
-%   Copyright (c) 2013 Mark Mikofski, 2015 Tom Holden
+function UpdateRepository( Directory, GitDirectory, Remote )
+    %
+    %   Copyright (c) 2013-2015 Mark Mikofski, 2015 Tom Holden
 
-% Git.init is a static method (so is clone) for obvious reasons
-initCMD = org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+    ShortDefaultGitDirectory = [ Directory '/.git' ];
+    LongDefaultGitDirectory = [ ShortDefaultGitDirectory '/' ];
 
-%% change message to reinitialized if gitDir already exists
-msg = 'Initialized';
-if exist( gitDirectory, 'dir' ) == 7
-    msg = 'Reinitialized';
-end
-%% set directory
-folder = java.io.File( directory );
-% Java always makes relative paths in matlab userpath
-if ~folder.isAbsolute
-    folder = java.io.File( pwd, directory ); % folder relative to pwd
-end
+    if exist( ShortDefaultGitDirectory, 'file' ) == 2
+        GitDirString = fileread( ShortDefaultGitDirectory );
+        CurrentGitDirectory = regexp( GitDirString, '(?<=^\s*gitdir:\s*)\S+(?=\s*$)', 'lineanchors', 'once', 'match' );
+    else
+        CurrentGitDirectory = LongDefaultGitDirectory;
+    end
 
-git_folder = java.io.File( gitDirectory );
-% Java always makes relative paths in matlab userpath
-if ~git_folder.isAbsolute
-    git_folder = java.io.File( pwd, gitDirectory ); % folder relative to pwd
-end
+    JavaDirectory = GetJavaFile( Directory );
+    JavaCurrentGitDirectory = GetJavaFile( CurrentGitDirectory );
 
-initCMD.readEnvironment;
-initCMD.setWorkTree( folder );
-initCMD.setGitDir( git_folder );
+    [ ~, RepositoryName ] = fileparts( Remote );
 
-%% call
-gitRepository = initCMD.build;
-gitAPI = org.eclipse.jgit.api.Git( gitRepository );
-gitRepository = gitAPI.getRepository( );
-%% output message
-gitDir = gitRepository.getDirectory;
-fprintf('%s Git repository in %s\n',msg,char(gitDir));
+    if exist( CurrentGitDirectory, 'dir' ) == 7
+        try
+            RepoBuilder = org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 
-config = gitRepository.getConfig;
+            RepoBuilder.readEnvironment;
+            RepoBuilder.setWorkTree( JavaDirectory );
+            RepoBuilder.setGitDir( JavaCurrentGitDirectory );
+            RepoBuilder.setMustExist( true );
 
-OriginSet = isempty( config.getString( 'remote', 'origin', 'url' ) );
+            % call
+            gitRepository = RepoBuilder.build;
+            gitAPI = org.eclipse.jgit.api.Git( gitRepository );
+            gitRepository = gitAPI.getRepository( );
 
-[ ~, RepositoryName ] = fileparts( remote );
+            config = gitRepository.getConfig;
+            RepoError = isempty( config.getString( 'remote', 'origin', 'url' ) );
 
-if OriginSet
-    disp( [ 'Setting new ' RepositoryName ' Git origin to: ' remote ] );
-    config.setString( 'remote', 'origin', 'url', remote );
-    config.save;
-    disp( [ 'Downloading the latest ' RepositoryName ' files.' ] );
-    fetchCMD = gitAPI.fetch;
-    fetchCMD.setProgressMonitor( com.mikofski.jgit4matlab.MATLABProgressMonitor );
-    fetchCMD.call;
-    disp( [ 'Updating local ' RepositoryName ' files from the downloaded ones.' ] );
-    resetCMD = gitAPI.reset;
-    resetCMD.setMode( org.eclipse.jgit.reset.ResetType.HARD );
-    resetCMD.setProgressMonitor( com.mikofski.jgit4matlab.MATLABProgressMonitor );
-    resetCMD.call;
-else
-    disp( [ 'Merging any local changes with the latest ' RepositoryName ' files.' ] );
-    pullCMD = gitAPI.pull;
-    pullCMD.setProgressMonitor( com.mikofski.jgit4matlab.MATLABProgressMonitor );
-    pullCMD.call;
+            if RepoError
+                disp( [ 'Old Git repository for ' RepositoryName ' did not have an origin URL. Recloning to repair.' ] );
+            else
+                pullCMD = gitAPI.pull;
+                pullCMD.setProgressMonitor( com.mikofski.jgit4matlab.MATLABProgressMonitor );
+                pullCMD.call;
+            end
+
+            disp( [ 'Succesfully updated the latest files from the ' RepositoryName ' repository.' ] );
+            CurrentGitDirectory = [];
+        catch CaughtRepoError
+            disp( [ 'Error ' CaughtRepoError.identifier 'accessing the Git repository for ' RepositoryName '. Details follow:' ] );
+            disp( CaughtRepoError.message );
+            RepoError = true; 
+        end
+        if RepoError
+            DestinationGitDirectory = [ tempname '/' ];
+            disp( [ 'Moving old Git directory from: ' CurrentGitDirectory ' to ' DestinationGitDirectory ] );
+            movefile( CurrentGitDirectory, DestinationGitDirectory, 'f' );
+        end
+    else
+        CurrentGitDirectory = [];    
+    end
+
+    if isempty( CurrentGitDirectory )
+        cloneCMD = org.eclipse.jgit.api.Git.clone;
+        cloneCMD.setDirectory( JavaDirectory );
+        cloneCMD.setBare( false );
+        cloneCMD.setCloneAllBranches( false );
+        cloneCMD.setCloneSubmodules( true );
+        cloneCMD.setNoCheckout( false );
+        cloneCMD.setProgressMonitor( com.mikofski.jgit4matlab.MATLABProgressMonitor );
+        cloneCMD.setURI( Remote );
+        cloneCMD.setBranch( 'master' );
+        cloneCMD.call( );
+        disp( [ 'Succesfully cloned the latest files from the ' RepositoryName ' repository.' ] );
+
+        CurrentGitDirectory = LongDefaultGitDirectory;
+    end
+
+    SourceDestEqual = false;
+    try
+        movefile( CurrentGitDirectory, GitDirectory, 'f' );
+    catch MoveError
+        if strcmp( MoveError.identifier, 'MATLAB:MOVEFILE:SourceAndDestinationSame' )
+            SourceDestEqual = true;
+        else
+            rethrow( MoveError );
+        end
+    end
+
+    if ~SourceDestEqual
+        FID = fopen( ShortDefaultGitDirectory, 'w' );
+        fprintf( FID, 'gitdir: %s', relativepath( GitDirectory, Directory ) );
+        fclose( FID );
+    end
+
 end
