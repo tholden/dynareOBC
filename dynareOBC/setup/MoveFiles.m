536bc6dce6301f2bd532ede89c6459db2334b5e3
tholden <thomas.holden@gmail.com>
2015-11-29 09:34:08 -0800

    stop auto update ever losing my work
    Former-commit-id: 8b679498518ba0a4c3440c0736d64b9163a4e265 [formerly d398f689336f3f4dc14a9443f39b6b40b02af417]
    Former-commit-id: 5582a1292d6ce1fe50d590f5563dc90cd6856e59

--- dynareOBC/setup/MoveFiles.m
+++ dynareOBC/setup/MoveFiles.m
@@ -18,20 +18,50 @@ function MoveFiles( Source, Destination )
         if File.isdir
             MoveFiles( [ Source File.name '/' ], [ Destination File.name '/' ] );
         else
-            try
-                copyfile( [ Source File.name ], [ Destination File.name ], 'f' );
-                lastwarn( '' );
-                WarningState = warning( 'off', 'all' );
-                delete( [ Source File.name ] );
-                warning( WarningState );
-                [ ~, DeleteWarningIdentifier ] = lastwarn;
-                if ~isempty( DeleteWarningIdentifier )
-                    disp( [ 'Warning ' DeleteWarningIdentifier ' moving: ' Source File.name ' to ' Destination File.name ] );
+            if exists( [ Destination File.name ], 'file' ) == 2
+                Different = true;
+                srcFile = -1;
+                destFile = -1;
+                try
+                    srcFile = fopen( [ Source File.name '/' ], 'r' );
+                    srcData = fread( srcFile, Inf, '*uint8' );
+                    destFile = fopen( [ Destination File.name '/' ], 'r' );
+                    destData = fread( destFile, Inf, '*uint8' );
+                    if numel( srcData ) == numel( destData ) && all( srcData == destData )
+                        Different = false;
+                    end
+                catch
                 end
-            catch MoveError
-                disp( [ 'Error ' MoveError.identifier ' moving: ' Source File.name ' to ' Destination File.name ] );
+                try
+                    fclose( srcFile );
+                catch
+                end
+                try
+                    fclose( destFile );
+                catch
+                end
+                if Different
+                    SafeMove( [ Destination File.name ], [ Destination File.name '.bak' ] );
+                end
             end
+            SafeMove( [ Source File.name ], [ Destination File.name ] );
         end
     end
     
+end
+
+function SafeMove( SourceFile, DestinationFile )
+    try
+        copyfile( SourceFile, DestinationFile, 'f' );
+        lastwarn( '' );
+        WarningState = warning( 'off', 'all' );
+        delete( SourceFile );
+        warning( WarningState );
+        [ ~, DeleteWarningIdentifier ] = lastwarn;
+        if ~isempty( DeleteWarningIdentifier )
+            disp( [ 'Warning ' DeleteWarningIdentifier ' moving: ' SourceFile ' to ' DestinationFile ] );
+        end
+    catch MoveError
+        disp( [ 'Error ' MoveError.identifier ' moving: ' SourceFile ' to ' DestinationFile ] );
+    end
 end
