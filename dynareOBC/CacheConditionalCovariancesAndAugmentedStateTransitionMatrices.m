tholden/dynareOBC

commit 346c9fcb6d3b848663c7b9fc0edd5b5d472e44a5

covariance matrix bug fix
Former-commit-id: 172eb2362e58feaeddf0c511b4d8d4cb4a749b93 [formerly b183a82be7ca625eaff625b9f1bd4c21660eee94]
Former-commit-id: fb15a8142b6d2f9c2d16c79177004f4ae273a114

dynareOBC/CacheConditionalCovariancesAndAugmentedStateTransitionMatrices.m
@@ -293,7 +293,7 @@ function dynareOBC = CacheConditionalCovariancesAndAugmentedStateTransitionMatri
                 % p and q are indexes of the lower triangle of a matrix, p>=q
                 pWeight = 0.5 * ( 1 + cos( pi * ( p - 1 ) / TM2 ) );
                 qWeight = 0.5 * ( 1 + cos( pi * ( q - 1 ) / TM2 ) );
-                CurrentCov = A1Powers{ p - q + 1 } * VarianceZ1{ q }; %#ok<PFBNS>
+                CurrentCov = A1Powers{ p - q + 1 } * VarianceZ1{ q } * A1Powers{ p - q + 1 }'; %#ok<PFBNS>
                 ReducedCov = full( CurrentCov( inv_order_var( VarIndices_ZeroLowerBounded ), inv_order_var( VarIndices_ZeroLowerBounded ) ) ); %#ok<PFBNS>
                 ConditionalCovarianceTemp{i} = ( pWeight * qWeight * 0.5 ) * ( ReducedCov + ReducedCov' );
             end

dynareOBC/RetrieveConditionalCovariances.m
@@ -74,7 +74,7 @@ function RootConditionalCovariance = RetrieveConditionalCovariances( options, oo
             % p and q are indexes of the lower triangle of a matrix, p>=q
             pWeight = 0.5 * ( 1 + cos( pi * ( p - 1 ) / TM2 ) );
             qWeight = 0.5 * ( 1 + cos( pi * ( q - 1 ) / TM2 ) );
-            CurrentCov = A2Powers{ p - q + 1 } * VarianceZ2{ q }; %#ok<PFBNS>
+            CurrentCov = A2Powers{ p - q + 1 } * VarianceZ2{ q } * A2Powers{ p - q + 1 }'; %#ok<PFBNS>
             ReducedCov = full( CurrentCov( inv_order_var( VarIndices_ZeroLowerBounded ), inv_order_var( VarIndices_ZeroLowerBounded ) ) ); %#ok<PFBNS>
             ConditionalCovarianceTemp{i} = ( pWeight * qWeight * 0.5 ) * ( ReducedCov + ReducedCov' );
         end

commit 7ff238641446d0b4ae1c900dfe9c349b7487645e

fixed bug introduced in 346c9fcb6d3b848663c7b9fc0edd5b5d472e44a5 [formerly fb15a8142b6d2f9c2d16c79177004f4ae273a114] [formerly 172eb2362e58feaeddf0c511b4d8d4cb4a749b93 [formerly b183a82be7ca625eaff625b9f1bd4c21660eee94]]
346c9fcb6d3b848663c7b9fc0edd5b5d472e44a5 [formerly fb15a8142b6d2f9c2d16c79177004f4ae273a114] [formerly 172eb2362e58feaeddf0c511b4d8d4cb4a749b93 [formerly b183a82be7ca625eaff625b9f1bd4c21660eee94]] allegedly fixed a bug, but
actually the code was correct to start with. there is now a comment
which makes clear why it was correct to start with.


Former-commit-id: ea18a1dbfef8a65af20f8b3810cf02bcf8a17dcb [formerly 0446d79d45253a4894fa50e47b7fcd4dd7648e3f]
Former-commit-id: a1d635103981aa9ecdf7f911bf3dd878b952ec78

dynareOBC/CacheConditionalCovariancesAndAugmentedStateTransitionMatrices.m
@@ -298,7 +298,7 @@ function dynareOBC = CacheConditionalCovariancesAndAugmentedStateTransitionMatri
                 p = floor( 0.5 * ( 1 + sqrt( 8 * i - 7 ) ) );
                 q = i - 0.5 * p * ( p - 1 );
                 % p and q are indexes of the lower triangle of a matrix, p>=q
-                CurrentCov = A1Powers{ p - q + 1 } * VarianceZ1{ q } * A1Powers{ p - q + 1 }'; %#ok<PFBNS>
+                CurrentCov = A1Powers{ p - q + 1 } * VarianceZ1{ q }; %#ok<PFBNS> % * A1Powers{ q - q + 1 }' = eye; 
                 ReducedCov = full( CurrentCov( inv_order_var( VarIndices_ZeroLowerBounded ), inv_order_var( VarIndices_ZeroLowerBounded ) ) ); %#ok<PFBNS>
                 ConditionalCovarianceTemp{i} = 0.5 * ( ReducedCov + ReducedCov' );
             end

dynareOBC/RetrieveConditionalCovariances.m
@@ -78,7 +78,7 @@ function RootConditionalCovariance = RetrieveConditionalCovariances( options, oo
             p = floor( 0.5 * ( 1 + sqrt( 8 * i - 7 ) ) );
             q = i - 0.5 * p * ( p - 1 );
             % p and q are indexes of the lower triangle of a matrix, p>=q
-            CurrentCov = A2Powers{ p - q + 1 } * VarianceZ2{ q } * A2Powers{ p - q + 1 }'; %#ok<PFBNS>
+            CurrentCov = A2Powers{ p - q + 1 } * VarianceZ2{ q }; %#ok<PFBNS> % * A2Powers{ p - q + 1 }' = eye
             ReducedCov = full( CurrentCov( inv_order_var( VarIndices_ZeroLowerBounded ), inv_order_var( VarIndices_ZeroLowerBounded ) ) ); %#ok<PFBNS>
             ConditionalCovarianceTemp{i} = 0.5 * ( ReducedCov + ReducedCov' );
         end