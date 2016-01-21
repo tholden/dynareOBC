tholden/dynareOBC

commit 7d9b2997ae25023f3ee9ae83367837c05f4068f3

small mlv simulation efficiency improvements
Former-commit-id: a5bfb1c2c124cf35866d76f7f47d5cb27f04ad89 [formerly 58160a51ee8489983d71dbc699bc5b43acfa7d20]
Former-commit-id: 22ee9802266404256c7cf34a24b8a291fe54ed59

dynareOBC/SimulateModel.m
@@ -262,6 +262,12 @@ function Simulation = SimulateModel( ShockSequence, M, options, oo, dynareOBC, D
             p = [];
         end
         
+        for i = 1 : nMLV
+            MLVName = MLVNames{i};
+            Simulation.MLVsWithBounds.( MLVName ) = NaN( SimulationLength, 1 );
+            Simulation.MLVsWithoutBounds.( MLVName ) = NaN( SimulationLength, 1 );
+        end
+        
         for t = 1 : SimulationLength
             % clear the last warning
             lastwarn( '' );
@@ -325,22 +331,18 @@ function Simulation = SimulateModel( ShockSequence, M, options, oo, dynareOBC, D
                     if WarningGenerated
                         warning( 'dynareOBC:InnerMLVWarning', 'Warnings were generated in the inner loop responsible for evaluating expectations of model local variables.' );
                     end
-                    for i = 1 : nMLV
-                        Simulation.MLVsWithBounds.( MLVNames{i} )( t ) = MLVValuesWithBounds( i );
-                        Simulation.MLVsWithoutBounds.( MLVNames{i} )( t ) = MLVValuesWithoutBounds( i );
-                    end
-               else
-                    CurrentMLVsWithBounds = dynareOBCTempGetMLVs( [ LagValuesWithBoundsLagIndices; CurrentValuesWithBoundsCurrentIndices; FutureValues ], CurrentShock, ParamVec, SteadyState, 1 );
+                else
+                    MLVValuesWithBounds = dynareOBCTempGetMLVs( [ LagValuesWithBoundsLagIndices; CurrentValuesWithBoundsCurrentIndices; FutureValues ], CurrentShock, ParamVec, SteadyState, 1 );
                     if dynareOBC.NumberOfMax > 0
-                        CurrentMLVsWithoutBounds = dynareOBCTempGetMLVs( [ LagValuesWithoutBoundsLagIndices; CurrentValuesWithoutBoundsCurrentIndices; FutureValues ], CurrentShock, ParamVec, SteadyState, 1 );
+                        MLVValuesWithoutBounds = dynareOBCTempGetMLVs( [ LagValuesWithoutBoundsLagIndices; CurrentValuesWithoutBoundsCurrentIndices; FutureValues ], CurrentShock, ParamVec, SteadyState, 1 );
                     else
-                        CurrentMLVsWithoutBounds = CurrentMLVsWithBounds;
+                        MLVValuesWithoutBounds = MLVValuesWithBounds;
                     end
-                    for i = 1 : nMLV
-                        MLVName = MLVNames{i};
-                        Simulation.MLVsWithBounds.( MLVName )( t ) = CurrentMLVsWithBounds.( MLVName );
-                        Simulation.MLVsWithoutBounds.( MLVName )( t ) = CurrentMLVsWithoutBounds.( MLVName );
-                    end
+                end
+                for i = 1 : nMLV
+                    MLVName = MLVNames{i};
+                    Simulation.MLVsWithBounds.( MLVName )( t ) = MLVValuesWithBounds.( MLVName );
+                    Simulation.MLVsWithoutBounds.( MLVName )( t ) = MLVValuesWithoutBounds.( MLVName );
                 end
                 LagValuesWithBounds = CurrentValuesWithBounds;
                 LagValuesWithoutBounds = CurrentValuesWithoutBounds;

commit 62a515d13061b52ed607482808beb3a34d3ac419

fix bug introduced in 7d9b2997ae25023f3ee9ae83367837c05f4068f3 [formerly 22ee9802266404256c7cf34a24b8a291fe54ed59] [formerly a5bfb1c2c124cf35866d76f7f47d5cb27f04ad89 [formerly 58160a51ee8489983d71dbc699bc5b43acfa7d20]]
Former-commit-id: dd5837aad39a4330827a8d8d2f8adddf453ef619 [formerly 95f4b03d3891991fea451bfbc35a41576b606317]
Former-commit-id: 8dc6900050a4a8ffb9ddfcb8487b437f54927340

dynareOBC/SimulateModel.m
@@ -264,8 +264,8 @@ function Simulation = SimulateModel( ShockSequence, M, options, oo, dynareOBC, D
         
         for i = 1 : nMLV
             MLVName = MLVNames{i};
-            Simulation.MLVsWithBounds.( MLVName ) = NaN( SimulationLength, 1 );
-            Simulation.MLVsWithoutBounds.( MLVName ) = NaN( SimulationLength, 1 );
+            Simulation.MLVsWithBounds.( MLVName ) = NaN( 1, SimulationLength );
+            Simulation.MLVsWithoutBounds.( MLVName ) = NaN( 1, SimulationLength );
         end
         
         for t = 1 : SimulationLength