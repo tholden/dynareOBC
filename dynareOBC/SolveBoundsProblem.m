tholden/dynareOBC

commit e82f14e1997631b65c3113b47719616246cca83b

solve bounds problem using optimizer
also fixes the erroneous commit 5ab3615ef8113dd46e78439b687e9e8700b09067 [formerly aab37e6bc089d913fce6c20a57c5b437517a96f4] [formerly 1a099fb2ef52e6a3a1a9aacc0f92f105d7cbc84b [formerly fba2bf40d9b14b9610ed58982dec48b780910f4e]]


Former-commit-id: c12c957d16ed8bfa1b4474edc23b58cbad553916 [formerly 5f7b00b48e7cc562c625a3c4a98e0d545b9a4c64]
Former-commit-id: df5ff4f4022e9044a2362a739eef5101b2917cc6

dynareOBC/SolveBoundsProblem.m
@@ -1,14 +1,19 @@
 function y = SolveBoundsProblem( q, dynareOBC )
-    Ts = dynareOBC.TimeToEscapeBounds;
-    
-    Optimizer = dynareOBC.Optimizer;
 
     Tolerance = dynareOBC.Tolerance;
+    
     if all( q >= -Tolerance ) && ~dynareOBC.FullHorizon
         y = dynareOBC.ZeroVecS;
         return
     end
     
+    Ts = dynareOBC.TimeToEscapeBounds;
+    Optimizer = dynareOBC.Optimizer;
+    M = dynareOBC.MMatrix;
+    ZeroVecS = dynareOBC.ZeroVecS;
+    sIndices = dynareOBC.sIndices;
+    ssIndices = dynareOBC.ssIndices;
+
     Norm_q = norm( q, Inf );
     if Norm_q < Tolerance
         Norm_q = 1;
@@ -18,8 +23,7 @@ function y = SolveBoundsProblem( q, dynareOBC )
     ParametricSolutionFound = dynareOBC.ParametricSolutionFound;
     
     if sum( ParametricSolutionFound ) > 0    
-        qsScaled = qScaled( dynareOBC.sIndices );    
-        ssIndices = dynareOBC.ssIndices;
+        qsScaled = qScaled( sIndices );    
     end
     
     if dynareOBC.FullHorizon
@@ -31,11 +35,11 @@ function y = SolveBoundsProblem( q, dynareOBC )
     for Tss = InitTss : Ts
     
         CParametricSolutionFound = ParametricSolutionFound( Tss );
+        CssIndices = ssIndices{ Tss };
         
         if CParametricSolutionFound > 0
             
             strTss = int2str( Tss );
-            CssIndices = ssIndices{ Tss };
             qssScaled = qsScaled( CssIndices );
             
             try
@@ -52,8 +56,10 @@ function y = SolveBoundsProblem( q, dynareOBC )
         end
         
         if CParametricSolutionFound == 0
-            
-            yScaled = value( yScaled ) / value( alpha );
+            OptOut = Optimizer{ [ qScaled; Tss ] };
+            yScaled = OptOut( 1 : ( end - 1 ), : );
+            alpha = OptOut( end );
+            yScaled = yScaled / alpha;
         end
         
         if all( isfinite( yScaled ) )
@@ -61,7 +67,7 @@ function y = SolveBoundsProblem( q, dynareOBC )
             y = ZeroVecS;
             y( CssIndices ) = yScaled;
             w = q + M * y;
-            if all( w >= -Tolerance ) && all( abs( w( dynareOBC.sIndices ) .* y ) <= Tolerance )
+            if all( w >= -Tolerance ) && all( abs( w( sIndices ) .* y ) <= Tolerance )
                 return;
             end
         end

commit e10bb8b8bf64c61d7a15a75af6e74a7199477747

fix bug introduced in e82f14e1997631b65c3113b47719616246cca83b [formerly df5ff4f4022e9044a2362a739eef5101b2917cc6] [formerly c12c957d16ed8bfa1b4474edc23b58cbad553916 [formerly 5f7b00b48e7cc562c625a3c4a98e0d545b9a4c64]]
Former-commit-id: b5b2a0a7804f4e8c7e9bdadf242f46b7e9e47691 [formerly 83f3b269b60a89939389cdca6d95c8575b385b7f]
Former-commit-id: 572cafdb296ef6d96845c619bb8e9994771544bb

dynareOBC/SolveBoundsProblem.m
@@ -10,9 +10,7 @@ function y = SolveBoundsProblem( q, dynareOBC )
     Ts = dynareOBC.TimeToEscapeBounds;
     Optimizer = dynareOBC.Optimizer;
     M = dynareOBC.MMatrix;
-    ZeroVecS = dynareOBC.ZeroVecS;
     sIndices = dynareOBC.sIndices;
-    ssIndices = dynareOBC.ssIndices;
 
     Norm_q = norm( q, Inf );
     if Norm_q < Tolerance
@@ -24,6 +22,8 @@ function y = SolveBoundsProblem( q, dynareOBC )
     
     if sum( ParametricSolutionFound ) > 0    
         qsScaled = qScaled( sIndices );    
+        ssIndices = dynareOBC.ssIndices;
+        ZeroVecS = dynareOBC.ZeroVecS;
     end
     
     if dynareOBC.FullHorizon
@@ -35,10 +35,10 @@ function y = SolveBoundsProblem( q, dynareOBC )
     for Tss = InitTss : Ts
     
         CParametricSolutionFound = ParametricSolutionFound( Tss );
-        CssIndices = ssIndices{ Tss };
         
         if CParametricSolutionFound > 0
             
+            CssIndices = ssIndices{ Tss };
             strTss = int2str( Tss );
             qssScaled = qsScaled( CssIndices );
             
@@ -52,6 +52,8 @@ function y = SolveBoundsProblem( q, dynareOBC )
                 warning( 'dynareOBC:ParametricEvaluationProblem', 'Problem running the parametric solution.' );
                 CParametricSolutionFound = 0;
             end
+            y = ZeroVecS;
+            y( CssIndices ) = yScaled;
             
         end
         
@@ -59,13 +61,11 @@ function y = SolveBoundsProblem( q, dynareOBC )
             OptOut = Optimizer{ [ qScaled; Tss ] };
             yScaled = OptOut( 1 : ( end - 1 ), : );
             alpha = OptOut( end );
-            yScaled = yScaled / alpha;
+            y = yScaled / alpha;
         end
         
-        if all( isfinite( yScaled ) )
-            yScaled = max( 0, yScaled * Norm_q );
-            y = ZeroVecS;
-            y( CssIndices ) = yScaled;
+        if all( isfinite( y ) )
+            y = max( 0, y * Norm_q );
             w = q + M * y;
             if all( w >= -Tolerance ) && all( abs( w( sIndices ) .* y ) <= Tolerance )
                 return;