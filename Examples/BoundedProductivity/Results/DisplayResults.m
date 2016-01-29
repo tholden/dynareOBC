46c4f5e0fad0341a805bf8b8d50837c7737da4c4
tholden <thomas.holden@gmail.com>
2016-01-28 19:57:00 -0800

    initial bounded productivity output
    Former-commit-id: 74af1307673279f046cdfe4eaaed0dce59a3a90d [formerly 71052eddf6112e581d1475537ab86758216d7c32]
    Former-commit-id: 12acaf4dc316d173da3bd67e3a7938a4d28f200d

---
+++ Examples/BoundedProductivity/Results/DisplayResults.m
@@ -0,0 +1,33 @@
+clear all; %#ok<CLALL>
+WarningState = warning( 'off', 'all' );
+try
+    Files = dir( '*.mat' );
+    Norms = zeros( length( Files ), 8 );
+    for i = 1 : length( Files )
+        
+        load( Files( i ).name );
+        
+        AtBound = oo_.endo_simul(end,:) <= 0.0001;
+        rError = dynareOBC_.MLVSimulationWithBounds.rError;
+        
+        AtBound = AtBound( 101:end );
+        rError = rError( 101:end );
+        
+        OneNorm = mean( abs( rError ) );
+        TwoNorm = sqrt( mean( rError .^ 2 ) );
+        InfNorm = max( abs( rError ) );
+        
+        AtBoundOneNorm = mean( abs( rError( AtBound ) ) );
+        AtBoundTwoNorm = sqrt( mean( rError( AtBound ) .^ 2 ) );
+        AtBoundInfNorm = max( abs( rError( AtBound ) ) );
+        
+        Norms( i, : ) = [ ( t2 - t1 ) / 1000, OneNorm, TwoNorm, InfNorm, AtBoundOneNorm, AtBoundTwoNorm, AtBoundInfNorm, sum( AtBound ) ];
+        
+    end
+    Norms = array2table( Norms, 'VariableNames', { 'Seconds', 'OneNorm', 'TwoNorm', 'InfNorm', 'AtBoundOneNorm', 'AtBoundTwoNorm', 'AtBoundInfNorm', 'PeriodsAtBound' }, 'RowNames', { Files.name } );
+    Norms = sortrows( Norms, 'OneNorm' );
+    disp( Norms );
+catch Err
+    disp( Err.message );
+end
+warning( WarningState );
