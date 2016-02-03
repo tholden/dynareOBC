tholden/dynareOBC

commit 148724911617030a8d8ecb1cda0087d15ebaf81e

efficiency improvements for spfind
Former-commit-id: c63384bf6b333c99f27a5914ad086e466c480e6f [formerly ed7fb880f18f688bcb9623daa9b7e9df4a3fb162]
Former-commit-id: 731295a79ee73f57dca81629c868086ad3d4f89e

dynareOBC/spfind.m
@@ -1,6 +1,5 @@
 function [ xi, xj, xs ] = spfind( x )
 
-   [ xi, xj, xs ] = find( x );
-   xs( abs( xs ) < eps ) = 0;
+   [ xi, xj, xs ] = find( abs( x ) > eps );
 
 end

commit 9748d974a04f74c7f0ce60b1e4d3460494ca6c1e

fix stupid bug introduced in 148724911617030a8d8ecb1cda0087d15ebaf81e [formerly 731295a79ee73f57dca81629c868086ad3d4f89e] [formerly c63384bf6b333c99f27a5914ad086e466c480e6f [formerly ed7fb880f18f688bcb9623daa9b7e9df4a3fb162]]
Former-commit-id: f403f3c6dd5fa0e76904454e7ec74afd8ab55a11 [formerly c856da1fbc08b9e4ce39528b5bd8ece4de2504c1]
Former-commit-id: 2b5f675761b9bd5825238ec714d4099902ae637d

dynareOBC/spfind.m
@@ -1,5 +1,6 @@
 function [ xi, xj, xs ] = spfind( x )
 
-   [ xi, xj, xs ] = find( abs( x ) > eps );
+    x( abs( x ) < eps ) = 0;
+    [ xi, xj, xs ] = find( x );
 
 end