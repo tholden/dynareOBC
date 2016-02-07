3e3ce7c70edf72868941c826085481804c9c411f
tholden <thomas.holden@gmail.com>
2016-02-07 14:03:55 +0000

    test simpler max
    Former-commit-id: d964798c955309402777fbd1a6d9f5d0482833de [formerly df4156afbddb4c283d51235bec5910d3abc8516b] [formerly 2c0ad4e8efff2d43d7956c4827af912cebf50733 [formerly cc9125701a41db8f9bca36ae65cbd89ebeb8778d]]
    Former-commit-id: 94f093807a41170b2d90fdc1268ace7dbb9dbcd0 [formerly 769120fe02aabb150b190290c719bd01f7c9ceee]
    Former-commit-id: 5887b1f7e12eda3777d2c05daa80e03026e7831f

--- Examples/CapitalConstraints/EvaluateValueFunctionAtPoint.m
+++ Examples/CapitalConstraints/EvaluateValueFunctionAtPoint.m
@@ -12,39 +12,40 @@ function [ Vnew, Cnew, CBnew ] = EvaluateValueFunctionAtPoint( k, a, Wv, kv, V, 
     
     CBnew = exp( HalleySolveBound( tlog( CBg ), kNewCore, MOMalphaDalphaPnu, thetaK ) );
     
-    Step = 1e-4;
-    Cg = min( CBnew * ( Cg / CBg ), CBnew );
-    LB = max( 0.5 * CBnew, Cg - Step );
-    UB = min( CBnew, Cg + Step );
-    Cg = 0.5 * ( LB + UB );
-    FLB = Maximand( LB, ODOPnu, OMalpha, AKEalpha, OPnuDalphaPnu, beta, thetaK, kNewCore, MOMalphaDalphaPnu, V, Wv, kv, nk );
-    FCg = Maximand( Cg, ODOPnu, OMalpha, AKEalpha, OPnuDalphaPnu, beta, thetaK, kNewCore, MOMalphaDalphaPnu, V, Wv, kv, nk );
-    FUB = NaN;
-    while FLB >= FCg
-        UB = Cg;
-        FUB = FCg;
-        Cg = LB;
-        FCg = FLB;
-        LB = max( 0.5 * LB, LB - Step );
-        Step = 2 * Step;
-        FLB = Maximand( LB, ODOPnu, OMalpha, AKEalpha, OPnuDalphaPnu, beta, thetaK, kNewCore, MOMalphaDalphaPnu, V, Wv, kv, nk );
-    end
-    if isnan( FUB )
-        FUB = Maximand( UB, ODOPnu, OMalpha, AKEalpha, OPnuDalphaPnu, beta, thetaK, kNewCore, MOMalphaDalphaPnu, V, Wv, kv, nk );
-    end
-    while ( FUB >= FCg ) && ( UB < CBnew )
-        if FUB > FCg
-            LB = Cg;
-            FLB = FCg;
-        end
-        Cg = UB;
-        FCg = FUB;
-        UB = min( UB + Step, CBnew );
-        Step = 2 * Step;
-        FUB = Maximand( UB, ODOPnu, OMalpha, AKEalpha, OPnuDalphaPnu, beta, thetaK, kNewCore, MOMalphaDalphaPnu, V, Wv, kv, nk );
-    end
+%     Step = 1e-4;
+%     Cg = min( CBnew * ( Cg / CBg ), CBnew );
+%     LB = max( 0.5 * CBnew, Cg - Step );
+%     UB = min( CBnew, Cg + Step );
+%     Cg = 0.5 * ( LB + UB );
+%     FLB = Maximand( LB, ODOPnu, OMalpha, AKEalpha, OPnuDalphaPnu, beta, thetaK, kNewCore, MOMalphaDalphaPnu, V, Wv, kv, nk );
+%     FCg = Maximand( Cg, ODOPnu, OMalpha, AKEalpha, OPnuDalphaPnu, beta, thetaK, kNewCore, MOMalphaDalphaPnu, V, Wv, kv, nk );
+%     FUB = NaN;
+%     while FLB >= FCg
+%         UB = Cg;
+%         FUB = FCg;
+%         Cg = LB;
+%         FCg = FLB;
+%         LB = max( 0.5 * LB, LB - Step );
+%         Step = 2 * Step;
+%         FLB = Maximand( LB, ODOPnu, OMalpha, AKEalpha, OPnuDalphaPnu, beta, thetaK, kNewCore, MOMalphaDalphaPnu, V, Wv, kv, nk );
+%     end
+%     if isnan( FUB )
+%         FUB = Maximand( UB, ODOPnu, OMalpha, AKEalpha, OPnuDalphaPnu, beta, thetaK, kNewCore, MOMalphaDalphaPnu, V, Wv, kv, nk );
+%     end
+%     while ( FUB >= FCg ) && ( UB < CBnew )
+%         if FUB > FCg
+%             LB = Cg;
+%             FLB = FCg;
+%         end
+%         Cg = UB;
+%         FCg = FUB;
+%         UB = min( UB + Step, CBnew );
+%         Step = 2 * Step;
+%         FUB = Maximand( UB, ODOPnu, OMalpha, AKEalpha, OPnuDalphaPnu, beta, thetaK, kNewCore, MOMalphaDalphaPnu, V, Wv, kv, nk );
+%     end
     
-    [ Cnew, Vnew ] = GoldenSectionMaximise( LB, UB, FLB, FUB, ODOPnu, OMalpha, AKEalpha, OPnuDalphaPnu, beta, thetaK, kNewCore, MOMalphaDalphaPnu, V, Wv, kv, nk );
+    FCBnew = Maximand( CBnew, ODOPnu, OMalpha, AKEalpha, OPnuDalphaPnu, beta, thetaK, kNewCore, MOMalphaDalphaPnu, V, Wv, kv, nk );
+    [ Cnew, Vnew ] = GoldenSectionMaximise( 0, CBnew, -Inf, FCBnew, ODOPnu, OMalpha, AKEalpha, OPnuDalphaPnu, beta, thetaK, kNewCore, MOMalphaDalphaPnu, V, Wv, kv, nk );
 end
 
 function cBnew = HalleySolveBound( cBg, kNewCore, MOMalphaDalphaPnu, thetaK )
