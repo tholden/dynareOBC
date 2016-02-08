d5d8db3916034041e13094d0702389f9f2558d4b
tholden <thomas.holden@gmail.com>
2016-02-08 11:00:54 +0000

    no need for manual compile of queryglobalsolution
    Former-commit-id: 4a36313f01b638629bb22c0d75b8e10664585932 [formerly 979ab9c7473918037e80f5d24e9d59dce1ad6f6e] [formerly 61c097711da439195a86dd14d519bc79a8f81aeb [formerly 8956f1297c7bfc39f6c5669d14472b74a18e29c0]]
    Former-commit-id: 7da72e8f35463d3a9d451cf1d72bbacda059250a [formerly cd9b356730b11dce47c56dadabba521b741311c0]
    Former-commit-id: 19f1939e41a7893077e5128297741026e78c1c54

--- Examples/CapitalConstraints/QueryGlobalSolution.m
+++ Examples/CapitalConstraints/QueryGlobalSolution.m
@@ -1,7 +1,22 @@
 function [ Cnew, DCnew, DDCnew ] = QueryGlobalSolution( k, a )
     persistent kv av V CB alpha beta nu theta rho sigma;
-    if isempty( kv )
-        Results = load( 'GlobalResults.mat' );
+    
+    if coder.target('MATLAB')
+        if isempty( kv )
+            Results = load( 'GlobalResults.mat' );
+            kv = Results.kv;
+            av = Results.av;
+            V = Results.V;
+            CB = Results.CB;
+            alpha = Results.alpha;
+            beta = Results.beta;
+            nu = Results.nu;
+            theta = Results.theta;
+            rho = Results.rho;
+            sigma = Results.sigma;
+        end
+    else
+        Results = coder.load( 'GlobalResults.mat' );
         kv = Results.kv;
         av = Results.av;
         V = Results.V;
@@ -13,7 +28,7 @@ function [ Cnew, DCnew, DDCnew ] = QueryGlobalSolution( k, a )
         rho = Results.rho;
         sigma = Results.sigma;
     end
-    [ ~, Cnew, ~ ] = EvaluateValueFunctionOffGrid_mex( k, a, kv, av, V, CB, alpha, beta, nu, theta, rho, sigma );
+    [ ~, Cnew, ~ ] = EvaluateValueFunctionOffGrid( k, a, kv, av, V, CB, alpha, beta, nu, theta, rho, sigma );
     DCnew = NaN( 2, 1 );
     DDCnew = NaN( 2, 2 );
 end
