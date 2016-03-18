4b67893ff3465ffc79f66481a8c3694d5b390ae7
tholden <thomas.holden@gmail.com>
2016-03-18 17:55:16 +0000

    correct return arguments

--- Examples/BorrowingConstraint/RunGlobalSolution.m
+++ Examples/BorrowingConstraint/RunGlobalSolution.m
@@ -1,4 +1,4 @@
 clear all; %#ok<CLALL>
 coder -build GlobalSolution.prj;
-[ V, C, CB, kv, av, alpha, beta, nu, theta, rho, sigma ] = GlobalSolution_mex;
+[ V, X, XB, Bv, Av, beta, mu, rho, sigma, Ybar, R ] = GlobalSolution_mex;
 save GlobalResults.mat;
