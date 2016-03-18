cec99e919befc3323b3142063530aaf4aa073fba
tholden <thomas.holden@gmail.com>
2016-03-18 17:23:59 +0000

    do not impose monotonicity on policy function

--- Examples/BorrowingConstraint/IterateValueFunction.m
+++ Examples/BorrowingConstraint/IterateValueFunction.m
@@ -7,14 +7,11 @@ function [ Vnew, Xnew ] = IterateValueFunction( V, X, XB, W, Bv, Av, beta, Ybar,
         A = Av( iA );
         Wv = W( :, iA );
         Vmax = -Inf;
-        Xmax = -Inf;
         for iB = 1 : nB
             B = Bv( iB ); %#ok<PFBNS>
-            [ Vtmp, Xtmp ] = EvaluateValueFunctionAtPoint( B, A, Wv, Bv, V, XB( iA, iB ), beta, Ybar, R );
+            [ Vtmp, Xnew( iA, iB ) ] = EvaluateValueFunctionAtPoint( B, A, Wv, Bv, V, XB( iA, iB ), beta, Ybar, R );
             Vmax = max( Vmax, Vtmp );
-            Xmax = max( Xmax, Xtmp );
             Vnew( iA, iB ) = Vmax;
-            Xnew( iA, iB ) = Xmax;
         end
     end
     Vnew = cummax( Vnew );
