4c8b48e4177099676d1540f275a13650c24ef2f4
Tom Holden <thomas.holden@gmail.com>
2016-03-19 17:33:22 +0000

    give up on enforcing positive third deriative

--- Examples/BorrowingConstraint/IterateValueFunction.m
+++ Examples/BorrowingConstraint/IterateValueFunction.m
@@ -30,21 +30,16 @@ function [ Vnew, Xnew, Verr ] = IterateValueFunction( V, X, XB, W, Bv, Av, beta,
             Xnew( iA, iB ) = Xmax;
         end
     end
-    Vnew = coder.nullcopy( Vtmp );
+    Vnew = cummax( Vtmp );
     parfor iA = 1 : nA
         V0min = 0;
         V1min = 0;
-        V2min = 0;
         for iB = nB : -1 : 1
-            if iB < nB - 1
-                V2min = min( V2min, Vtmp( iA, iB ) - V0min - V1min );
-                V1min = V1min + V2min; % = min( V1min + V2min, Vtmp( iA, iB ) - V0min) );
-                V0min = V0min + V1min; % = min( V0min + V1min + V2min, Vtmp( iA, iB ) );
-            elseif iB == nB - 1
-                V1min = min( 0, Vtmp( iA, iB ) - V0min );
-                V0min = V0min + V1min; % = min( V0min, Vtmp( iA, iB ) );
+            if iB < nB
+                V1min = min( V1min, Vnew( iA, iB ) - V0min );
+                V0min = V0min + V1min; % = min( V0min + V1min, Vtmp( iA, iB ) );
             else % iB == nB
-                V0min = Vtmp( iA, iB );
+                V0min = Vnew( iA, iB );
             end
             Vnew( iA, iB ) = V0min;
         end
