58fbd526c181101998834f4de4a9ceb70ec14cf8
Tom Holden <thomas.holden@gmail.com>
2016-03-19 14:05:20 +0000

    impose monotonicity of second derivatives

--- Examples/BorrowingConstraint/IterateValueFunction.m
+++ Examples/BorrowingConstraint/IterateValueFunction.m
@@ -34,11 +34,21 @@ function [ Vnew, Xnew, Verr ] = IterateValueFunction( V, X, XB, W, Bv, Av, beta,
     parfor iA = 1 : nA
         V0max = -Inf;
         V1min = Inf;
-        % V2max = -Inf;
+        V2max = -Inf;
         for iB = 1 : nB
-            V0maxNew = max( V0max, min( V0max + V1min, Vtmp( iA, iB ) ) );
-            V1min = min( V1min, V0maxNew - V0max );
-            V0max = V0maxNew;
+            % V1minNew = V1min + V2maxNew >= V1min + V2max
+            % V1minNew <= V1min
+            % V0maxNew = V0max + V1minNew
+            % V0max + V1min + V2max <= V0maxNew <= V0max + V1min
+            % V0maxNew = max( V0max + V1min + V2max, min( V0max + V1min, Vtmp( iA, iB ) ) );
+            % V1minNew = V0maxNew - V0max; % V1minNew = max( V1min + V2max, min( V1min, Vtmp( iA, iB ) - V0max ) );
+            % V2maxNew = V1minNew - V1min; % V2maxNew = max( V2max, min( 0, Vtmp( iA, iB ) - V0max - V1min ) );
+            % V0max = V0maxNew;
+            % V1min = V1minNew;
+            % V2max = V2maxNew;
+            V2max = max( V2max, min( 0, Vtmp( iA, iB ) - V0max - V1min ) );
+            V1min = V1min + V2max;
+            V0max = V0max + V1min;
             Vnew( iA, iB ) = V0max;
         end
     end
