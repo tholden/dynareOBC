b46af5cc8c629c5df770aaef43a64e005562d548
Tom Holden <thomas.holden@gmail.com>
2016-03-20 18:14:24 +0000

    save correct outputs

--- Examples/BorrowingConstraint/RunGlobalSolution.m
+++ Examples/BorrowingConstraint/RunGlobalSolution.m
@@ -1,4 +1,4 @@
 clear all; %#ok<CLALL>
 coder -build GlobalSolution.prj;
-[ V, X, PP, BS, XB, Bv, Av, beta, mu, rho, sigma, Ybar, R ] = GlobalSolution_mex;
+[ V, X, PP, BS, VS, XB, Bv, Av, beta, mu, rho, sigma, Ybar, R ] = GlobalSolution_mex;
 save GlobalResults.mat;
