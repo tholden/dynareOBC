0d27208e4ef5903c43f4dba46f96bdd1969a2744
tholden <thomas.holden@gmail.com>
2016-04-12 17:02:09 +0100

    tighter optim tolerance

--- dynareOBC/FMinConWrapper.m
+++ dynareOBC/FMinConWrapper.m
@@ -1,5 +1,5 @@
 function [ x, f ] = FMinConWrapper( OptiFunction, x, lb, ub, varargin )
-    Options = optimoptions( 'fmincon', 'Display', 'iter-detailed', 'MaxFunEvals', Inf, 'MaxIter', Inf, 'TolCon', eps, 'TypicalX', x, 'UseParallel', true, 'ObjectiveLimit', -Inf );
+    Options = optimoptions( 'fmincon', 'Display', 'iter-detailed', 'MaxFunEvals', Inf, 'MaxIter', Inf, 'TolCon', eps, 'OptimalityTolerance', eps, 'TypicalX', x, 'UseParallel', true, 'ObjectiveLimit', -Inf );
     for i = 1 : 2 : length( varargin )
         Options.( varargin{ i } ) = varargin{ i + 1 };
     end
