565448691c6d5e16c515273a71f0d12c40cad50e
Tom Holden <thomas.holden@gmail.com>
2016-04-15 00:18:29 +0100

    better initialisation of typicalx

--- dynareOBC/FMinConWrapper.m
+++ dynareOBC/FMinConWrapper.m
@@ -1,7 +1,16 @@
-function [ x, f ] = FMinConWrapper( OptiFunction, x, lb, ub, varargin )
-    Options = optimoptions( 'fmincon', 'Display', 'iter-detailed', 'MaxFunEvals', Inf, 'MaxIter', Inf, 'TolCon', eps, 'OptimalityTolerance', eps, 'TypicalX', x, 'UseParallel', true, 'ObjectiveLimit', -Inf );
+function [ x, f ] = FMinConWrapper( OptiFunction, x, LB, UB, varargin )
+    TypicalX = abs( x );
+    SelectFiniteLB = isfinite( LB );
+    SelectFiniteUB = isfinite( UB );
+    TypicalX( SelectFiniteLB ) = max( TypicalX( SelectFiniteLB ), abs( LB( SelectFiniteLB ) ) );
+    TypicalX( SelectFiniteUB ) = max( TypicalX( SelectFiniteUB ), abs( UB( SelectFiniteUB ) ) );
+    TypicalX = max( sqrt( eps ), TypicalX );
+    SelectNegativeX = x < 0;
+    TypicalX( SelectNegativeX ) = -TypicalX( SelectNegativeX );
+    
+    Options = optimoptions( 'fmincon', 'Display', 'iter-detailed', 'MaxFunEvals', Inf, 'MaxIter', Inf, 'TolCon', eps, 'OptimalityTolerance', eps, 'TypicalX', TypicalX, 'UseParallel', true, 'ObjectiveLimit', -Inf );
     for i = 1 : 2 : length( varargin )
         Options.( varargin{ i } ) = varargin{ i + 1 };
     end
-    [ x, f ] = fmincon( OptiFunction, x, [], [], [], [], lb, ub, [], Options );
+    [ x, f ] = fmincon( OptiFunction, x, [], [], [], [], LB, UB, [], Options );
 end
