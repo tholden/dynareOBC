e20e09c814bd651b58704533413db7c772befdf6
Tom Holden <thomas.holden@gmail.com>
2016-04-15 19:18:49 +0100

    optimality tolerance is not supported in older matlab versions

--- dynareOBC/FMinConWrapper.m
+++ dynareOBC/FMinConWrapper.m
@@ -26,7 +26,7 @@ function [ x, f ] = FMinConWrapper( OptiFunction, x, LB, UB, varargin )
         end
     end
     
-    Options = optimoptions( 'fmincon', 'Display', 'iter-detailed', 'MaxFunEvals', Inf, 'MaxIter', Inf, 'TolCon', eps, 'OptimalityTolerance', eps, 'TypicalX', TypicalX, 'UseParallel', true, 'ObjectiveLimit', -Inf );
+    Options = optimoptions( 'fmincon', 'Display', 'iter-detailed', 'MaxFunEvals', Inf, 'MaxIter', Inf, 'TolCon', eps, 'TolFun', eps, 'TypicalX', TypicalX, 'UseParallel', true, 'ObjectiveLimit', -Inf );
     for i = 1 : 2 : length( varargin )
         Options.( varargin{ i } ) = varargin{ i + 1 };
     end
