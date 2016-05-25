a6ff781f6588a88e2cc963879e31e763bd6da18e
tholden <thomas.holden@gmail.com>
2016-05-25 17:22:25 +0100

    try catch in runextendedpathversion

--- Tests/ComparisonOfPerfectForesightSolutionsForLinearModels/RunExtendedPathVersion.m
+++ Tests/ComparisonOfPerfectForesightSolutionsForLinearModels/RunExtendedPathVersion.m
@@ -3,8 +3,14 @@ try
 catch
 end
 close all force;
-delete ExtendedPathVersion*.mat *.log ExtendedPathVersion.m ExtendedPathVersion_*.*
-rmdir ExtendedPathVersion s
+try
+    delete ExtendedPathVersion*.mat *.log ExtendedPathVersion.m ExtendedPathVersion_*.*
+catch
+end
+try
+    rmdir ExtendedPathVersion s
+catch
+end
 ShockSequence = oo_.exo_simul';
 EPEndoSequence = oo_.endo_simul(:,2:end);
 save ExtendedPathResults.mat ShockSequence EPEndoSequence
