e53a0ff83d8991af3f4c7a0eafc69b0da48a12f9
tholden <thomas.holden@gmail.com>
2016-05-27 15:59:57 +0100

    updated comparison now EP is fixed

--- Tests/ComparisonOfPerfectForesightSolutionsForLinearModels/RunAllAndCompare.m
+++ Tests/ComparisonOfPerfectForesightSolutionsForLinearModels/RunAllAndCompare.m
@@ -1,5 +1,9 @@
 RunExtendedPathVersion;
 RunOccBinVersion;
 RunDynareOBCVersion;
+clear all; %#ok<CLALL>
+load ExtendedPathResults.mat
+load OccBinResults.mat
+load DynareOBCResults.mat DOBCEndoSequence
 figure(1); plot(EPEndoSequence'-DOBCEndoSequence');
 figure(2); plot(OBEndoSequence'-DOBCEndoSequence');
