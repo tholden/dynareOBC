de745db8508ffc76888872d9271b4158b8e72759
tholden <thomas.holden@gmail.com>
2016-05-28 16:26:56 +0100

    make runallandcompare work on dynare 4.4.3

--- Tests/ComparisonOfPerfectForesightSolutionsForLinearModels/RunAllAndCompare.m
+++ Tests/ComparisonOfPerfectForesightSolutionsForLinearModels/RunAllAndCompare.m
@@ -1,16 +1,11 @@
 RunExtendedPathVersion;
-OccBinOK = true;
-try
-    RunOccBinVersion;
-catch
-    OccBinOK = false;
-end
+RunOccBinVersion;
 RunDynareOBCVersion;
 clear all; %#ok<CLALL>
 load ExtendedPathResults.mat
 load DynareOBCResults.mat DOBCEndoSequence
 figure(1); plot(EPEndoSequence'-DOBCEndoSequence');
-if OccBinOK
-    load OccBinResults.mat OBEndoSequence
+load OccBinResults.mat OBEndoSequence
+if ~isempty( OBEndoSequence )
     figure(2); plot(OBEndoSequence'-DOBCEndoSequence');
 end
--- Tests/ComparisonOfPerfectForesightSolutionsForLinearModels/RunOccBinVersion.m
+++ Tests/ComparisonOfPerfectForesightSolutionsForLinearModels/RunOccBinVersion.m
@@ -1,10 +1,17 @@
-load ExtendedPathResults.mat;
-addpath( [ fileparts( which( 'dynare' ) ) '\occbin' ] );
-[ ~, OBEndoSequence ] = solve_one_constraint( 'OccBinVersionSteady', 'OccBinVersionBound', 'd<-(1-beta)', 'd>-(1-beta)', ShockSequence', 'e', length( ShockSequence + 1000 ), 100 );
-beta = 0.99;
-OBEndoSequence = bsxfun( @plus, OBEndoSequence', [ 0; 0; 1 - beta; 1 - beta; 0 ] );
-close all force;
-delete OccBinVersion*.mat *.log OccBinVersion*.m OccBinVersion*_*.*
-rmdir OccBinVersionSteady s
-rmdir OccBinVersionBound s
+try
+    load ExtendedPathResults.mat;
+    addpath( [ fileparts( which( 'dynare' ) ) '\occbin' ] );
+    [ ~, OBEndoSequence ] = solve_one_constraint( 'OccBinVersionSteady', 'OccBinVersionBound', 'd<-(1-beta)', 'd>-(1-beta)', ShockSequence', 'e', length( ShockSequence + 1000 ), 100 );
+    beta = 0.99;
+    OBEndoSequence = bsxfun( @plus, OBEndoSequence', [ 0; 0; 1 - beta; 1 - beta; 0 ] );
+catch
+    OBEndoSequence = [];
+end
+try
+    close all force;
+    delete OccBinVersion*.mat *.log OccBinVersion*.m OccBinVersion*_*.*
+    rmdir OccBinVersionSteady s
+    rmdir OccBinVersionBound s
+catch
+end
 save OccBinResults.mat OBEndoSequence
