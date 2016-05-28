c6c488b19dc7719b29da6a0289cb3456e5cba5a9
tholden <thomas.holden@gmail.com>
2016-05-28 16:06:52 +0100

    make runallandcompare work on dyanre 4.4.3

--- Tests/ComparisonOfPerfectForesightSolutionsForLinearModels/RunAllAndCompare.m
+++ Tests/ComparisonOfPerfectForesightSolutionsForLinearModels/RunAllAndCompare.m
@@ -1,9 +1,16 @@
 RunExtendedPathVersion;
-RunOccBinVersion;
+OccBinOK = true;
+try
+    RunOccBinVersion;
+catch
+    OccBinOK = false;
+end
 RunDynareOBCVersion;
 clear all; %#ok<CLALL>
 load ExtendedPathResults.mat
-load OccBinResults.mat
 load DynareOBCResults.mat DOBCEndoSequence
 figure(1); plot(EPEndoSequence'-DOBCEndoSequence');
-figure(2); plot(OBEndoSequence'-DOBCEndoSequence');
+if OccBinOK
+    load OccBinResults.mat OBEndoSequence
+    figure(2); plot(OBEndoSequence'-DOBCEndoSequence');
+end
