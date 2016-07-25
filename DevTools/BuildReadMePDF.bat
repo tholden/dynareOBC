8bba64324b7f6bde4677cbdc0020f3fe43c1ca37
Tom Holden <thomas.holden@gmail.com>
2016-07-25 17:03:16 +0300

    prettier readme.pdf

--- DevTools/BuildReadMePDF.bat
+++ DevTools/BuildReadMePDF.bat
@@ -1,3 +1,4 @@
 cd ..
-set path=%path%;C:\Program Files (x86)\Pandoc
-pandoc README.md -f markdown_github -o ReadMe.pdf
+set path=%path%;C:\Program Files (x86)\Pandoc;C:\Program Files\wkhtmltopdf\bin
+pandoc README.md -f markdown_github -o ReadMe.pdf -N --toc --wrap=none -V papersize=A4 -V fontsize=10pt -V lang=en-GB -V documentclass=article -V margin-left=2.54cm -V margin-right=2.54cm -V margin-top=2.54cm -V margin-bottom=2.54cm -V fontfamily=tgpagella -V links-as-notes -V colorlinks
+cd DevTools
