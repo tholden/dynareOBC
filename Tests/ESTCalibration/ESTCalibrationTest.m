7a24c0bb402341a33456afedf7f9207933735c67
tholden <thomas.holden@gmail.com>
2017-01-21 23:50:43 +0000

    use cubature in est test

--- Tests/ESTCalibration/ESTCalibrationTest.m
+++ Tests/ESTCalibration/ESTCalibrationTest.m
@@ -1,7 +1,8 @@
+clear all;
 addpath ../../Core
 
-T = 100000;
-N = 10;
+Order = 14;
+N = 3;
 
 xi = 10 * randn( N, 1 );
 RootOmega = 0.1 * randn( N, N );
@@ -11,39 +12,41 @@ tau = randn ^ 2;
 nu = 8.5 + 4 * randn ^ 2;
 log_nuM4 = log( nu - 4 );
 
-PhiN0 = rand( 1, T );
-PhiN10 = rand( 1, T );
-pTmp = randn( N, T );
+[ Weights, pTmp, T ] = fwtpts( N + 2, Order );
+disp( T );
+PhiN0 = normcdf( pTmp( end - 1, : ) );
+PhiN10 = normcdf( pTmp( end, : ) );
 
 FInvScaledInvChi = sqrt( 0.5 * ( nu + 1 ) ./ gammaincinv( PhiN10, 0.5 * ( nu + 1 ), 'upper' ) );
 tcdf_tau_nu = tcdf( tau, nu );
 FInvEST = tinv( tcdf_tau_nu + ( 1 - tcdf_tau_nu ) * PhiN0, nu );
 N11Scaler = FInvScaledInvChi .* sqrt( ( nu + FInvEST .^ 2 ) / ( 1 + nu ) );
 
-ESTPoints = bsxfun( @plus, RootOmega * bsxfun( @times, pTmp, N11Scaler ) + bsxfun( @times, delta, FInvEST ), xi );
+ESTPoints = bsxfun( @plus, RootOmega * bsxfun( @times, pTmp( 1:(end-2), : ), N11Scaler ) + bsxfun( @times, delta, FInvEST ), xi );
 
-mu = mean( ESTPoints, 2 );
-Sigma = cov( ESTPoints' );
-
+mu = sum( bsxfun( @times, ESTPoints, Weights ), 2 );
 DemeanedESTPoints = bsxfun( @minus, ESTPoints, mu );
+Weighted_DemeanedESTPoints = bsxfun( @times, DemeanedESTPoints, Weights );
 
-lambda = mu + delta * median( delta' * DemeanedESTPoints ); % slight cheat to get lambda
+Sigma = DemeanedESTPoints * Weighted_DemeanedESTPoints';
+Sigma = 0.5 * ( Sigma + Sigma' );
+
+lambda = ESTPoints( :, 1 );
 
 Zcheck = ( ( mu - lambda )' * DemeanedESTPoints ) / sqrt( ( mu - lambda )' * Sigma * ( mu - lambda ) );
 
-disp( mean( Zcheck ) );
-disp( mean( Zcheck.^2 ) );
+disp( Zcheck * Weights' );
+disp( Zcheck.^2 * Weights' );
 
 Zcheck = Zcheck - mean( Zcheck );
 Zcheck = Zcheck / sqrt( mean( Zcheck.^2 ) );
 
 hist( Zcheck, 100 );
 
-sZ3 = mean( Zcheck.^3 );
-sZ4 = mean( Zcheck.^4 );
+sZ3 = Zcheck.^3 * Weights';
+sZ4 = Zcheck.^4 * Weights';
 
 disp( [ sZ3 sZ4 ] );
-disp( [ std( Zcheck.^3 ) std( Zcheck.^4 ) ] );
 
 out1 = fsolve( @( in ) CalibrateMomentsEST( in( 1 ), in( 2 ), mu, lambda, Sigma, sZ3, sZ4 ), [ tau; log_nuM4 ], optimoptions( @fsolve, 'display', 'iter' ) );
 out2 = fsolve( @( in ) CalibrateMomentsEST( in( 1 ), log_nuM4, mu, lambda, Sigma, sZ3, [] ), tau, optimoptions( @fsolve, 'display', 'iter' ) );
