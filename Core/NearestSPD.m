7598d20dd0199e7117b87a1730d54c2061d51c81
tholden <thomas.holden@gmail.com>
2017-01-23 13:05:37 +0000

    tidy up, fix complex scalar case, return cholesky root

--- Core/NearestSPD.m
+++ Core/NearestSPD.m
@@ -1,4 +1,4 @@
-% Copyright (c) 2013, John D'Errico
+% Copyright (c) 2013, John D'Errico & 2016, Tom Holden
 % All rights reserved.
 % 
 % Redistribution and use in source and binary forms, with or without
@@ -24,62 +24,72 @@
 % POSSIBILITY OF SUCH DAMAGE.
 % 
 
-function Ahat = NearestSPD(A)
-% nearestSPD - the nearest (in Frobenius norm) Symmetric Positive Definite matrix to A
-% usage: Ahat = nearestSPD(A)
-%
-% From Higham: "The nearest symmetric positive semidefinite matrix in the
-% Frobenius norm to an arbitrary real matrix A is shown to be (B + H)/2,
-% where H is the symmetric polar factor of B=(A + A')/2."
-%
-% http://www.sciencedirect.com/science/article/pii/0024379588902236
-%
-% arguments: (input)
-%  A - square matrix, which will be converted to the nearest Symmetric
-%    Positive Definite Matrix.
-%
-% Arguments: (output)
-%  Ahat - The matrix chosen as the nearest SPD matrix to A.
+function [ Ahat, cholAhat ] = NearestSPD(A)
+    % nearestSPD - the nearest (in Frobenius norm) Symmetric Positive Definite matrix to A
+    % usage: Ahat = nearestSPD(A)
+    %
+    % From Higham: "The nearest symmetric positive semidefinite matrix in the
+    % Frobenius norm to an arbitrary real matrix A is shown to be (B + H)/2,
+    % where H is the symmetric polar factor of B=(A + A')/2."
+    %
+    % http://www.sciencedirect.com/science/article/pii/0024379588902236
+    %
+    % arguments: (input)
+    %  A - square matrix, which will be converted to the nearest Symmetric
+    %    Positive Definite Matrix.
+    %
+    % Arguments: (output)
+    %  Ahat - The matrix chosen as the nearest SPD matrix to A.
+    %  cholAhat - The cholesky root of Ahat
 
-if nargin ~= 1
-  error('Exactly one argument must be provided.')
-end
+    if nargin ~= 1
+      error('Exactly one argument must be provided.')
+    end
 
-% test for a square matrix A
-[r,c] = size(A);
-if r ~= c
-  error('A must be a square matrix.')
-elseif (r == 1)
-  % A was scalar
-  Ahat = max( A, eps );
-  return
-end
+    % test for a square matrix A
+    [r,c] = size(A);
+    if r ~= c
+      error('A must be a square matrix.')
+    elseif (r == 1)
+      % A was scalar
+      Ahat = max( real( A ), eps );
+      cholAhat = sqrt( Ahat );
+      return
+    end
 
-% symmetrize A into B
-B = (A + A')/2;
+    % symmetrize A into B
+    Ahat = (A + A')/2;
 
-% Compute the symmetric polar factor of B. Call it H.
-% Clearly H is itself SPD.
-[~,Sigma,V] = svd(B);
-H = V*Sigma*V';
+    [cholAhat,p] = chol(Ahat);
 
-% get Ahat in the above formula
-Ahat = (B+H)/2;
+    if p ~= 0
 
-% ensure symmetry
-Ahat = (Ahat + Ahat')/2;
+        % Compute the symmetric polar factor of B. Call it H.
+        % Clearly H is itself SPD.
+        [~,Sigma,V] = svd(Ahat);
+        H = V*Sigma*V';
 
-% test that Ahat is in fact PD. if it is not so, then tweak it just a bit.
-p = 1;
-k = 0;
-while p ~= 0
-  [~,p] = chol(Ahat);
-  k = k + 1;
-  if p ~= 0
-    % Ahat failed the chol test. It must have been just a hair off,
-    % due to floating point trash, so it is simplest now just to
-    % tweak by adding a tiny multiple of an identity matrix.
-    mineig = min(eig(Ahat));
-    Ahat = Ahat + (-mineig*k.^2 + eps(mineig))*eye(size(A));
-  end
+        % get Ahat in the above formula
+        Ahat = (Ahat+H)/2;
+
+        % ensure symmetry
+        Ahat = (Ahat + Ahat')/2;
+
+        % test that Ahat is in fact PD. if it is not so, then tweak it just a bit.
+        p = 1;
+        k = 0;
+        while p ~= 0
+          [cholAhat,p] = chol(Ahat);
+          k = k + 1;
+          if p ~= 0
+            % Ahat failed the chol test. It must have been just a hair off,
+            % due to floating point trash, so it is simplest now just to
+            % tweak by adding a tiny multiple of an identity matrix.
+            mineig = min(eig(Ahat));
+            Ahat = Ahat + (-mineig*k.^2 + eps(mineig))*eye(size(A));
+          end
+        end
+
+    end
+
 end
