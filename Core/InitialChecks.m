72d01c1c0b00afae862673a5ba4bb8f4f2d5d364
Tom Holden <thomas.holden@gmail.com>
2017-02-05 10:30:10 +0000

    last check from Li and Wu paper

--- Core/InitialChecks.m
+++ Core/InitialChecks.m
@@ -444,6 +444,12 @@ function dynareOBC = InitialChecks( dynareOBC )
                                         CPMatrix = true;
                                     end
                                 end
+                                if ~CPMatrix && rank( IminusMsc ) == Tss % theorem 3.1 of https://www.cogentoa.com/article/10.1080/23311835.2016.1271268.pdf
+                                    IMscAltRatio = IminusMsc \ IplusMsc;
+                                    if min( svd( IMscAltRatio ) ) > 1
+                                        CPMatrix = true;
+                                    end
+                                end
                             end
                         end
                     end
