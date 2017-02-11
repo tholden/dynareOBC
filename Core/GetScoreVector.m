ea6135fbd04acdb389630357a18a8a81c533d398
Tom Holden <thomas.holden@gmail.com>
2017-02-11 17:39:27 +0000

    fix incorrect scaling in GetScoreVector.m

--- Core/GetScoreVector.m
+++ Core/GetScoreVector.m
@@ -1,4 +1,3 @@
 function [ ObservationLikelihoods, EstimationPersistentState ] = GetScoreVector( p, EstimationPersistentState )
     [ ~, EstimationPersistentState, ObservationLikelihoods ] = EstimationObjective( p, EstimationPersistentState, false );
-    ObservationLikelihoods = -0.5 * ObservationLikelihoods;
 end
