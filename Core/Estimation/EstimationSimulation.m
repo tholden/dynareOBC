ef70572ede4009af1e65925d74c2a4ddd3e86304
Tom Holden <thomas.holden@gmail.com>
2017-02-14 10:31:57 +0000

    more work on moving  estimation to a separate module

--- Core/Estimation/EstimationSimulation.m
+++ Core/Estimation/EstimationSimulation.m
@@ -1,24 +1,26 @@
-function [ StateSimulation, ControlSimulation, MeasurementSimulation ] = EstimationSimulation( Parameters, PersistentState, ShockSequence )
+function [ PersistentState, StateSimulation, FullSimulation, MeasurementSimulation ] = EstimationSimulation( Parameters, PersistentState, ShockSequence )
 
     global M_ options_ oo_ dynareOBC_
     
+    SelectAugStateVariables = PersistentState.SelectAugStateVariables;
+    
     try
-        StatDistSimulation = SimulateModel( ShockSequence, false, [], true, true );
+        FullSimulationStruct = SimulateModel( ShockSequence, false, [], true, true );
     catch Error
         rethrow( Error );
     end
 
     if dynareOBC_.Order == 1
-        StatDistPoints = StatDistSimulation.first + StatDistSimulation.bound_offset;
+        FullSimulation = FullSimulationStruct.first + FullSimulationStruct.bound_offset;
     elseif dynareOBC_.Order == 2
-        StatDistPoints = [ StatDistSimulation.first; StatDistSimulation.second + StatDistSimulation.bound_offset ];
+        FullSimulation = [ FullSimulationStruct.first; FullSimulationStruct.second + FullSimulationStruct.bound_offset ];
     else
-        StatDistPoints = [ StatDistSimulation.first; StatDistSimulation.second; StatDistSimulation.first_sigma_2; StatDistSimulation.third + StatDistSimulation.bound_offset ];
+        FullSimulation = [ FullSimulationStruct.first; FullSimulationStruct.second; FullSimulationStruct.first_sigma_2; FullSimulationStruct.third + FullSimulationStruct.bound_offset ];
     end
     
-    StatDistPoints = StatDistPoints( SelectAugStateVariables, : );
+    StateSimulation = FullSimulation( SelectAugStateVariables, : );
     
-    if any( ~isfinite( StatDistPoints ) )
+    if any( ~isfinite( FullSimulation ) )
         error( 'dynareOBC:EstimationNonFiniteSimultation', 'Non-finite values were encountered during simulation.' );
     end
         
