b587708c6df38f0d79bae1bc72a792bbda4191f8
tholden <thomas.holden@gmail.com>
2017-08-28 21:57:31 +0100

    use the DoubleDouble class in calculating RobustDeterminants without VPA

--- Core/MChecks/RobustDeterminant.m
+++ Core/MChecks/RobustDeterminant.m
@@ -1,5 +1,6 @@
-function [ DetA, LB, UB ] = RobustDeterminant( A )
+function [ DetA, LB, UB ] = RobustDeterminant( InputA )
 
+    A = DoubleDouble( InputA );
     n = min( size( A, 1 ), size( A, 2 ) );
     A = A( 1:n, 1:n );
 
@@ -16,7 +17,7 @@ function [ DetA, LB, UB ] = RobustDeterminant( A )
     a = max( abs( A(:) ) );
     l = max( abs( L(:) ) );
     u = max( abs( U(:) ) );
-    eA = ( a + n * l * u ) * eps;
+    eA = ( a + n * l * u ) * DoubleDouble.eps;
     A1 = norm( A, 1 );
     AInf = norm( A, Inf );
     Aq = min( [ A1, AInf, sqrt( A1 * AInf ) ] );
@@ -25,10 +26,14 @@ function [ DetA, LB, UB ] = RobustDeterminant( A )
     UB = DetA + ed;
     
     if LB < 0 && UB > 0
-        k = rank( A );
+        k = rank( InputA );
         if k < n
             DetA = 0;
         end
     end
+    
+    DetA = double( DetA );
+    LB = double( LB );
+    UB = double( UB );
     
 end
