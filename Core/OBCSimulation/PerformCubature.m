tholden/dynareOBC

commit e95a0fc040d75768515425f7aa5073d9fddd24bd

solve initial point within cubature loop, plus other performance improvements

Core/OBCSimulation/PerformCubature.m
@@ -11,7 +11,6 @@ function [ y, GlobalVarianceShare ] = PerformCubature( y, UnconstrainedReturnPat
         CubatureWeights = ones( NumPoints, 1 ) * ( 1 / NumPoints );
         wTemp = 0.5 * sqrt( 2 * NumPoints );
         CubaturePoints = [ zeros( d, 1 ), eye( d ) * wTemp, eye( d ) * (-wTemp) ];
-        CubatureOrder = 1;
     elseif dynareOBC.QuasiMonteCarloLevel > 0
         CubatureOrder = dynareOBC.QuasiMonteCarloLevel;
         NumPoints = 2 .^ ( 2 : ( 1 + CubatureOrder ) ) - 1;
@@ -44,7 +43,7 @@ function [ y, GlobalVarianceShare ] = PerformCubature( y, UnconstrainedReturnPat
         p = [];
     end
 
-    yMatrix = y * CubatureWeights( 1, : );
+    yMatrix = zeros( size( y, 1 ), size( CubatureWeights, 2 ) );
 
     if ~isempty( p )
         p.progress;
@@ -52,22 +51,40 @@ function [ y, GlobalVarianceShare ] = PerformCubature( y, UnconstrainedReturnPat
     
     CubatureAcceleration = dynareOBC.CubatureAcceleration;
     CubatureTolerance = dynareOBC.CubatureTolerance;
+    if dynareOBC.FastCubature
+        CubatureTolerance = 0;
+    end
     PositiveCubatureTolerance = CubatureTolerance > 0;
     MaxCubatureSerialLoop = dynareOBC.MaxCubatureSerialLoop;
     
-    if PositiveCubatureTolerance
-        iMax = CubatureOrder;
+    global MatlabPoolSize
+    if DisableParFor || isempty( MatlabPoolSize )
+        LocalMatlabPoolSize = 1;
     else
+        LocalMatlabPoolSize = MatlabPoolSize;
+    end
+    CumNumPoints = cumsum( NumPoints );
+    CumNumPoints( end ) = LocalMatlabPoolSize;
+    iMin = find( CumNumPoints >= LocalMatlabPoolSize, 1 );
+    
+    if PositiveCubatureTolerance
+        iMax = length( NumPoints );
+    else
+        iMin = 1;
         iMax = 1;
     end
     
     WarningGenerated = false;
-    for i = 1 : iMax
+    for i = iMin : iMax
     
         if PositiveCubatureTolerance
-            jv = ( NumPoints( i ) + 1 ) : NumPoints( i + 1 );
+            if i == iMin
+                jv = 1 : NumPoints( i );
+            else
+                jv = ( NumPoints( i - 1 ) + 1 ) : NumPoints( i );
+            end
         else
-            jv = 2 : NumPoints( end );
+            jv = 1 : NumPoints( end );
         end
         if DisableParFor || length( jv ) <= MaxCubatureSerialLoop
             for j = jv
@@ -107,9 +124,9 @@ function [ y, GlobalVarianceShare ] = PerformCubature( y, UnconstrainedReturnPat
 
         if PositiveCubatureTolerance 
             if CubatureAcceleration
-                yNew = WynnEpsilonTransformation( yMatrix( :, 1 : ( i + 1 ) ) );
+                yNew = WynnEpsilonTransformation( yMatrix( :, 1 : i ) );
             else
-                yNew = yMatrix( :, i + 1 );
+                yNew = yMatrix( :, i );
             end
 
             yError = max( abs( y - yNew ) );

Core/OBCSimulation/FastIRFs.m
@@ -50,14 +50,16 @@ function [ y, TempIRFStruct ] = FastIRFsInternal( Shock, ShockName, M, oo, dynar
 
     UnconstrainedReturnPath = vec( TempIRFStruct.total( dynareOBC.VarIndices_ZeroLowerBounded, : )' );
     
-    y = SolveBoundsProblem( UnconstrainedReturnPath );
-
 	if dynareOBC.NumberOfMax > 0
-        if ~dynareOBC.NoCubature
-			[ y, GlobalVarianceShare ] = PerformCubature( y, UnconstrainedReturnPath, oo, dynareOBC, TempIRFStruct.first, false, [ 'Computing required integral for fast IRFs for ' ShockName '. Please wait for around ' ], '. Progress: ', [ 'Computing required integral for fast IRFs for ' ShockName '. Completed in ' ] );
+        if dynareOBC.NoCubature
+            y = SolveBoundsProblem( UnconstrainedReturnPath );
+        else
+            [ y, GlobalVarianceShare ] = PerformCubature( UnconstrainedReturnPath, oo, dynareOBC, TempIRFStruct.first, false, [ 'Computing required integral for fast IRFs for ' ShockName '. Please wait for around ' ], '. Progress: ', [ 'Computing required integral for fast IRFs for ' ShockName '. Completed in ' ] );
             if dynareOBC.Global
                 y = SolveGlobalBoundsProblem( y, GlobalVarianceShare, UnconstrainedReturnPath, TempIRFStruct.total( dynareOBC.VarIndices_ZeroLowerBoundedLongRun, : )', dynareOBC );
             end
         end
+    else
+        y = [];
 	end
 end

Core/OBCSimulation/SimulateModel.m
@@ -228,15 +228,15 @@ function Simulation = SimulateModel( ShockSequence, DisplayProgress, InitialFull
 
                     yOld = y;
                     try
-                        y = SolveBoundsProblem( UnconstrainedReturnPath );
-                        [ WarningMessages, WarningIDs, WarningPeriods ] = UpdateWarningList( t, WarningMessages, WarningIDs, WarningPeriods );
-
-                        if ~dynareOBC_.NoCubature
-                            [ y, GlobalVarianceShare ] = PerformCubature( y, UnconstrainedReturnPath, oo_, dynareOBC_, ReturnPathStruct.first, DisableParFor );
+                        if dynareOBC_.NoCubature
+                            y = SolveBoundsProblem( UnconstrainedReturnPath );
+                        else
+                            [ y, GlobalVarianceShare ] = PerformCubature( UnconstrainedReturnPath, oo_, dynareOBC_, ReturnPathStruct.first, DisableParFor );
                             if dynareOBC_.Global
                                 y = SolveGlobalBoundsProblem( y, GlobalVarianceShare, UnconstrainedReturnPath, ReturnPath( dynareOBC_.VarIndices_ZeroLowerBoundedLongRun, : )', dynareOBC_ );
                             end
                         end
+                        [ WarningMessages, WarningIDs, WarningPeriods ] = UpdateWarningList( t, WarningMessages, WarningIDs, WarningPeriods );
                     catch Error
                         if dynareOBC_.IgnoreBoundFailures
                             Reshaped_yOld = reshape( yOld, Ts, ns );

commit 4b72e5c1cc85736bc6ca298fd1c9a62536161719

fix bug introduced in e95a0fc040d75768515425f7aa5073d9fddd24bd

Core/OBCSimulation/PerformCubature.m
@@ -1,4 +1,4 @@
-function [ y, GlobalVarianceShare ] = PerformCubature( y, UnconstrainedReturnPath, oo, dynareOBC, FirstOrderSimulation, DisableParFor, varargin )
+function [ y, GlobalVarianceShare ] = PerformCubature( UnconstrainedReturnPath, oo, dynareOBC, FirstOrderSimulation, DisableParFor, varargin )
    
     [ RootConditionalCovariance, GlobalVarianceShare ] = RetrieveConditionalCovariances( oo, dynareOBC, FirstOrderSimulation );
     d = size( RootConditionalCovariance, 2 );
@@ -43,7 +43,7 @@ function [ y, GlobalVarianceShare ] = PerformCubature( y, UnconstrainedReturnPat
         p = [];
     end
 
-    yMatrix = zeros( size( y, 1 ), size( CubatureWeights, 2 ) );
+    yMatrix = zeros( dynareOBC.TimeToEscapeBounds * dynareOBC.NumberOfMax, size( CubatureWeights, 2 ) );
 
     if ~isempty( p )
         p.progress;