27c9ebb0a30e71715fe5bb90aa36a027bbfbc466
Tom Holden <thomas.holden@gmail.com>
2018-06-27 13:22:34 +0100

    work around for MSVC redistributable registry bug

--- Core/Setup/CheckMSVCRequirement.m
+++ Core/Setup/CheckMSVCRequirement.m
@@ -32,6 +32,27 @@ function DLLInstalled = CheckMSVCRequirement( MajorVersion, DesiredBuild, Platfo
         disp( [ 'Running ' ExePath '.' ] );
         fprintf( '\n' );
         system( [ 'start "Installing DynareOBC requirement" /wait "' dynareOBCPath '/Extern/Requirements/' ExePath '" /passive /norestart' ] );
+        
+        if DesiredBuild == 26429
+            % Double check that install took place.
+            Build = int32( 0 );
+            try
+                Build = winqueryreg( 'HKEY_LOCAL_MACHINE', [ 'SOFTWARE\Microsoft\VisualStudio\' MajorVersion  '\VC\Runtimes\' Platform '\' ], 'Bld' );
+            catch
+            end
+            if Build < int32( DesiredBuild )
+                try
+                    Build = winqueryreg( 'HKEY_LOCAL_MACHINE', [ 'SOFTWARE\Wow6432Node\Microsoft\VisualStudio\' MajorVersion '\VC\Runtimes\' Platform '\' ], 'Bld' );
+                catch
+                end
+            end
+            if Build < int32( DesiredBuild )
+                fprintf( '\n' );
+                disp( 'Due to a bug in the latest MSVC redistributable, the redistributable is not being detected. As a result, we will now install the required registry key.' );
+                fprintf( '\n' );
+                system( [ 'start "Correcting registry" /wait regedit "' dynareOBCPath '/Core/Setup/MSVCDetectionFix_' Platform '.reg"' ] );
+            end
+        end
         DLLInstalled = true;
     else
         DLLInstalled = false;
