7f5e9e61e745afd7e7d4c3e2b66712e3b7a825ba
Tom Holden <thomas.holden@gmail.com>
2018-09-25 12:21:42 +0200

    update pandoc command for latest pandoc

--- DevTools/BuildReadMes.m
+++ DevTools/BuildReadMes.m
@@ -48,7 +48,7 @@ try
         addpath( 'C:\Program Files (x86)\Pandoc' );
     catch
     end
-    !pandoc README.md -f markdown_github -o ReadMe.pdf -N --toc --wrap=none --latex-engine=xelatex -V papersize=A4 -V fontsize=10pt -V lang=en-GB -V documentclass=article -V margin-left=2.54cm -V margin-right=2.54cm -V margin-top=2.54cm -V margin-bottom=2.54cm -V mainfont=TeXGyrePagella -V sansfont=TeXGyreAdventor -V monofont=TeXGyreCursor -V links-as-notes -V colorlinks
+    !pandoc README.md -f gfm -o ReadMe.pdf -N --toc --wrap=none --pdf-engine=xelatex -V papersize=A4 -V fontsize=10pt -V lang=en-GB -V documentclass=article -V margin-left=2.54cm -V margin-right=2.54cm -V margin-top=2.54cm -V margin-bottom=2.54cm -V mainfont=TeXGyrePagella -V sansfont=TeXGyreAdventor -V monofont=TeXGyreCursor -V links-as-notes -V colorlinks
 catch Error
     disp( Error );
 end
