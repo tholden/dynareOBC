e777bd1b3f1654c9fb517c35bc76934e3af3dd11
Tom Holden <thomas.holden@gmail.com>
2018-10-02 16:36:21 +0200

    display m matrix rather than opening it

--- Examples/ArbitraryMMatrix/RunExample.m
+++ Examples/ArbitraryMMatrix/RunExample.m
@@ -1,7 +1,7 @@
 disp( 'This script illustrates that we can construct a model with a specific M matrix.' );
 
-dynareOBC ArbitraryM.mod TimeToEscapeBounds=10 TimeToReturnToSteadyState=10
+dynareOBC ArbitraryM.mod TimeToEscapeBounds=7 TimeToReturnToSteadyState=7
 
-disp( 'Opening M matrix. Notice that it has the numbers we specified in the MOD file.' );
+disp( 'The model''s M matrix follows. Notice that it has the numbers we specified in the MOD file.' );
 
-openvar dynareOBC_.MMatrix;
+disp( dynareOBC_.MMatrix( 1:7, 1:7 ) );
