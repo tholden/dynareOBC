92d0f896bc9298d695efaf5a0bd55a2c36e66953
Tom Holden <thomas.holden@gmail.com>
2018-10-02 16:52:27 +0200

    script to run the bkw2012 example

---
+++ Examples/BraunKorberWaki2012/RunExample.m
@@ -0,0 +1,6 @@
+disp( 'This script illustrates IRFs to a 5 standard deviation shock in the Braun Koerber Waki 2012 model.' );
+
+dynareOBC bkw2012 ShockScale=5
+
+disp( 'Note that both shocks hit the zero lower bound.' );
+disp( 'Note also from the output above that M is a P-matrix for this model, so there is a unique solution to the linearised model with the bound.' );
