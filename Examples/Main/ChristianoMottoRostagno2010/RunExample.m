1d1830b276df24db2b1656c1b5be7546d06966b8
Tom Holden <thomas.holden@gmail.com>
2018-10-03 19:01:22 +0200

    cmr script work in progress

--- Examples/Main/ChristianoMottoRostagno2010/RunExample.m
+++ Examples/Main/ChristianoMottoRostagno2010/RunExample.m
@@ -1,1 +1,31 @@
-disp( 'This script illustrates the IRFs to 20 standard deviation shocks in the Christiano Motto Rostangno (2010) paper.' );
+disp( 'This script illustrates the IRFs to 20 standard deviation shocks in the Christiano Motto Rostangno (2014) paper.' );
+
+FigureHandles = findall( groot, 'Type', 'figure' );
+OldFigures = zeros( length( FigureHandles ), 1 );
+for i = 1 : length( FigureHandles )
+    OldFigures( i ) = FigureHandles(i).Number;
+end
+
+dynareOBC cmr.mod ShockScale=20
+
+FigureHandles = findall( groot, 'Type', 'figure' );
+NewFigures1 = zeros( length( FigureHandles ), 1 );
+for i = 1 : length( FigureHandles )
+    NewFigures1( i ) = FigureHandles(i).Number;
+end
+NewFigures1 = setdiff( NewFigures1, OldFigures );
+
+dynareOBC cmr.mod ShockScale=20 TimeToEscapeBounds=128 SkipQuickPCheck SkipFirstSolutions=1
+
+FigureHandles = findall( groot, 'Type', 'figure' );
+NewFigures2 = zeros( length( FigureHandles ), 1 );
+for i = 1 : length( FigureHandles )
+    NewFigures2( i ) = FigureHandles(i).Number;
+end
+NewFigures2 = setdiff( NewFigures2, [ OldFigures; NewFigures1 ] );
+
+if length( NewFigures1 ) == length( NewFigures2 )
+else
+    disp( 'There was an unexpected number of figures. Did you close one while the code was running?' );
+    disp( 'Skipping plotting of Christiano Motto Rostangno (2014) IRF comparison.' );
+end
