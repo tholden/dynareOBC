558c4f7679b19c94afd4d5399721651155832c49
Tom Holden <thomas.holden@gmail.com>
2018-10-05 14:10:06 +0200

    turn off warnings while preparing sub matrices

--- Core/MChecks/PrepareNormalizedSubMatrices.m
+++ Core/MChecks/PrepareNormalizedSubMatrices.m
@@ -16,91 +16,98 @@ function dynareOBC = PrepareNormalizedSubMatrices( dynareOBC, SlowMode )
     
     CPMatrix = true;
     
-    for Tss = 1 : Ts
-        CssIndices = vec( bsxfun( @plus, (1:Tss)', 0:Ts:((ns-1)*Ts) ) )';
-        dynareOBC.ssIndices{ Tss } = CssIndices;
+    WarningState = warning( 'off', 'all' );
+    try
+        for Tss = 1 : Ts
+            CssIndices = vec( bsxfun( @plus, (1:Tss)', 0:Ts:((ns-1)*Ts) ) )';
+            dynareOBC.ssIndices{ Tss } = CssIndices;
 
-        Mc = dynareOBC.MMatrix( :, CssIndices );
-        Msc = dynareOBC.MsMatrix( CssIndices, CssIndices );
-        [ ~, ~, d2 ] = NormalizeMatrix( Msc, NormalizeTolerance, NormalizeTolerance );
-        Mc = bsxfun( @times, Mc, d2 );
-        d1 = 1 ./ CleanSmallVector( max( abs( Mc ), [], 2 ), NormalizeTolerance );
-        Mc = bsxfun( @times, d1, Mc );
-        Msc = Mc( sIndices( CssIndices ), : );
-        
-        dynareOBC.NormalizedSubMMatrices{ Tss } = Mc;
-        dynareOBC.NormalizedSubMsMatrices{ Tss } = Msc;
-        dynareOBC.d1SubMMatrices{ Tss } = d1;
-        dynareOBC.d1sSubMMatrices{ Tss } = d1( sIndices( CssIndices ) );
-        dynareOBC.d2SubMMatrices{ Tss } = d2';
-        
-        if ~CPMatrix
-            continue;
-        end
-        
-        CPMatrix = false;
-        
-        if any( diag( Msc ) <= 0 )
-            continue;
-        end
-        
-        [ ~, pMsc ] = chol( Msc + Msc' );
-        if pMsc == 0
-            CPMatrix = true;
-        else
-            TssTns = Tss * ns;
-        
-            if TssTns > 1 && any( abs( angle( eig( Msc ) ) ) >= pi - pi / TssTns )
+            Mc = dynareOBC.MMatrix( :, CssIndices );
+            Msc = dynareOBC.MsMatrix( CssIndices, CssIndices );
+            [ ~, ~, d2 ] = NormalizeMatrix( Msc, NormalizeTolerance, NormalizeTolerance );
+            Mc = bsxfun( @times, Mc, d2 );
+            d1 = 1 ./ CleanSmallVector( max( abs( Mc ), [], 2 ), NormalizeTolerance );
+            Mc = bsxfun( @times, d1, Mc );
+            Msc = Mc( sIndices( CssIndices ), : );
+
+            dynareOBC.NormalizedSubMMatrices{ Tss } = Mc;
+            dynareOBC.NormalizedSubMsMatrices{ Tss } = Msc;
+            dynareOBC.d1SubMMatrices{ Tss } = d1;
+            dynareOBC.d1sSubMMatrices{ Tss } = d1( sIndices( CssIndices ) );
+            dynareOBC.d2SubMMatrices{ Tss } = d2';
+
+            if ~CPMatrix
                 continue;
             end
-        
-            CompanionMsc = -abs( Msc );
-            CompanionMsc = CompanionMsc - 2 * diag( diag( CompanionMsc ) );
-            if min( min( inv( CompanionMsc ) ) ) >= 0 % H matrix check
+
+            CPMatrix = false;
+
+            if any( diag( Msc ) <= 0 )
+                continue;
+            end
+
+            [ ~, pMsc ] = chol( Msc + Msc' );
+            if pMsc == 0
                 CPMatrix = true;
             else
-                IminusMsc = eye( TssTns ) - Msc;
-                absIminusMsc = abs( IminusMsc );
-                if max( abs( eig( absIminusMsc ) ) ) < 1 % corollary 3.2 of https://www.cogentoa.com/article/10.1080/23311835.2016.1271268.pdf
+                TssTns = Tss * ns;
+
+                if TssTns > 1 && any( abs( angle( eig( Msc ) ) ) >= pi - pi / TssTns )
+                    continue;
+                end
+
+                CompanionMsc = -abs( Msc );
+                CompanionMsc = CompanionMsc - 2 * diag( diag( CompanionMsc ) );
+                if min( min( inv( CompanionMsc ) ) ) >= 0 % H matrix check
                     CPMatrix = true;
                 else
-                    IplusMsc = eye( TssTns ) + Msc;
-                    norm_absIminusMsc = norm( absIminusMsc );
-                    [ ~, pIMscComb ] = chol( IplusMsc' * IplusMsc - ( norm_absIminusMsc * norm_absIminusMsc ) * eye( TssTns ) );
-                    if pIMscComb == 0 % theorem 3.4 of https://www.cogentoa.com/article/10.1080/23311835.2016.1271268.pdf
+                    IminusMsc = eye( TssTns ) - Msc;
+                    absIminusMsc = abs( IminusMsc );
+                    if max( abs( eig( absIminusMsc ) ) ) < 1 % corollary 3.2 of https://www.cogentoa.com/article/10.1080/23311835.2016.1271268.pdf
                         CPMatrix = true;
                     else
-                        if norm_absIminusMsc < min( svd( IplusMsc ) ) % theorem 3.2 of https://www.cogentoa.com/article/10.1080/23311835.2016.1271268.pdf
+                        IplusMsc = eye( TssTns ) + Msc;
+                        norm_absIminusMsc = norm( absIminusMsc );
+                        [ ~, pIMscComb ] = chol( IplusMsc' * IplusMsc - ( norm_absIminusMsc * norm_absIminusMsc ) * eye( TssTns ) );
+                        if pIMscComb == 0 % theorem 3.4 of https://www.cogentoa.com/article/10.1080/23311835.2016.1271268.pdf
                             CPMatrix = true;
                         else
-                            if rank( IplusMsc ) == TssTns
-                                try
-                                    IMscRatio = IplusMsc \ IminusMsc;
-                                    if max( abs( eig( abs( IMscRatio ) ) ) ) < 1 || norm( IplusMsc \ IminusMsc ) < 1 % theorem 3.1 of https://www.cogentoa.com/article/10.1080/23311835.2016.1271268.pdf
-                                        CPMatrix = true;
+                            if norm_absIminusMsc < min( svd( IplusMsc ) ) % theorem 3.2 of https://www.cogentoa.com/article/10.1080/23311835.2016.1271268.pdf
+                                CPMatrix = true;
+                            else
+                                if rank( IplusMsc ) == TssTns
+                                    try
+                                        IMscRatio = IplusMsc \ IminusMsc;
+                                        if max( abs( eig( abs( IMscRatio ) ) ) ) < 1 || norm( IplusMsc \ IminusMsc ) < 1 % theorem 3.1 of https://www.cogentoa.com/article/10.1080/23311835.2016.1271268.pdf
+                                            CPMatrix = true;
+                                        end
+                                    catch
                                     end
-                                catch
                                 end
-                            end
-                            if ~CPMatrix && rank( IminusMsc ) == TssTns % theorem 3.1 of https://www.cogentoa.com/article/10.1080/23311835.2016.1271268.pdf
-                                try
-                                    IMscAltRatio = IminusMsc \ IplusMsc;
-                                    if min( svd( IMscAltRatio ) ) > 1
-                                        CPMatrix = true;
+                                if ~CPMatrix && rank( IminusMsc ) == TssTns % theorem 3.1 of https://www.cogentoa.com/article/10.1080/23311835.2016.1271268.pdf
+                                    try
+                                        IMscAltRatio = IminusMsc \ IplusMsc;
+                                        if min( svd( IMscAltRatio ) ) > 1
+                                            CPMatrix = true;
+                                        end
+                                    catch
                                     end
-                                catch
                                 end
                             end
                         end
                     end
                 end
             end
+
+            if CPMatrix
+                LargestPMatrix = Tss;
+            end
         end
-        
-        if CPMatrix
-            LargestPMatrix = Tss;
-        end
+    catch Error
+        warning( WarningState );
+        rethrow( Error );
     end
+    warning( WarningState );
     
     dynareOBC.LargestPMatrix = LargestPMatrix;
     
