c570f09f551c89a3bfb8e92016e35072130136c5
Tom Holden <thomas.holden@gmail.com>
2018-10-05 19:55:43 +0200

    price level target version

--- Examples/AdjemianDarracqPariesMoyen2007/RunExample.m
+++ Examples/AdjemianDarracqPariesMoyen2007/RunExample.m
@@ -4,3 +4,11 @@ dynareOBC SWNLWCD ShockScale=24.5 SkipFirstSolutions=1
 
 disp( 'Observe that the dotted line does not hit the bound, but the solid line does.' );
 disp( 'Observe too that the welfare costs of such a jump to the bound are about 5 times higher.' );
+
+disp( 'We now run the same exercise with a 0.01 response to the price level in the Taylor rule.' );
+disp( 'Press a key to continue:' );
+pause;
+
+dynareOBC SWNLWCD_PLT ShockScale=24.5 SkipFirstSolutions=1
+
+disp( 'Observe that now no additional solution was found, and the welfare consequences of the shock are greatly muted.' );
