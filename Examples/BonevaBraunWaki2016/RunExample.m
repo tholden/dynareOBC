a897b65e0de839b0d70a56b7e5c9cd6223514e35
Tom Holden <thomas.holden@gmail.com>
2018-10-06 16:18:17 +0200

    update runexample for file name change etc

--- Examples/BonevaBraunWaki2016/RunExample.m
+++ Examples/BonevaBraunWaki2016/RunExample.m
@@ -1,6 +1,6 @@
-disp( 'This script illustrates IRFs to 5 standard deviation shocks in the Braun Koerber Waki (2012) model.' );
+disp( 'This script illustrates IRFs to 5 standard deviation shocks in the Boneva Braun Waki (2016) model.' );
 
-dynareOBC bkw2012 ShockScale=5
+dynareOBC bbw2016.mod ShockScale=5
 
 disp( 'Note that both shocks hit the zero lower bound.' );
 disp( 'Note also from the output above that M is a P-matrix for this model, so there is a unique solution to the linearised model with the bound.' );
