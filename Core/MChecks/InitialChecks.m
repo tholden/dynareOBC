c1ebde3aa5426a827628d9ab310d1ccc4f933706
Tom Holden <thomas.holden@gmail.com>
2018-10-06 17:59:50 +0200

    note that the infinite tests can give inconclusive results with some solvers

--- Core/MChecks/InitialChecks.m
+++ Core/MChecks/InitialChecks.m
@@ -121,7 +121,7 @@ function dynareOBC = InitialChecks( dynareOBC )
         
         [ iValues, jValues ] = meshgrid( 1:FTGC, 1:FTGC );
         
-        LoopMessage = sprintf( 'M did not pass either the sufficient condition to be an S matrix for infinite T, or the sufficient condition to not be an S matrix for infinite T.\nTo discover the properties of M, try reruning with higher TimeToEscapeBounds.\n' );
+        LoopMessage = sprintf( 'M did not pass either the sufficient condition to be an S matrix for infinite T, or the sufficient condition to not be an S matrix for infinite T.\nTo discover the properties of M, try reruning with higher TimeToEscapeBounds, or try installing an alternative LP solver.\n' );
         
         try
             OpenPool;
