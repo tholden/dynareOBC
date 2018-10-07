05b08a01948c681d40acdfbc3734a0811790fb72
Tom Holden <thomas.holden@gmail.com>
2018-10-07 12:10:18 +0200

    correct message

--- Examples/SmetsWouters2003/RunExample.m
+++ Examples/SmetsWouters2003/RunExample.m
@@ -2,7 +2,7 @@ disp( 'This script illustrates properties of the Smets Wouters (2003) model.' );
 
 GeneratePlots;
 
-disp( 'Would you like to see results on determinacy of the Smets Wouters (2003) model modified to include NGDP targeting? (These are very slow, and require at least 32GB of RAM.)' );
+disp( 'Would you like to see results on determinacy of the Smets Wouters (2003) model modified to include a response to the price level? (These are very slow, and require at least 32GB of RAM.)' );
 Input = strtrim( lower( input( 'Press y then return to see them or just return to skip: ', 's' ) ) );
 if ( length( Input ) ~= 1 ) || ( Input( 1 ) ~= 'y' )
     return
