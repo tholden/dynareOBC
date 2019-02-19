dea102945dfa8dd5fa26817c89ba1247961f05e6
Tom Holden <thomas.holden@gmail.com>
2019-02-19 20:18:13 +0100

    bug fix: with multiple nested constraints, the GetMaxArgValues file was not correctly generated

--- Core/MODProcessing/Generate_dynareOBCTempGetMaxArgValues.m
+++ Core/MODProcessing/Generate_dynareOBCTempGetMaxArgValues.m
@@ -1,13 +1,12 @@
 function Generate_dynareOBCTempGetMaxArgValues( NumberOfMax, FileName )
-    StringNumberOfMax = int2str( NumberOfMax );
     FileText = fileread( [ FileName '.m' ] );
     FileText = regexprep( FileText, FileName, 'dynareOBCTempGetMaxArgValues' );
     FileText = regexprep( FileText, '\[(\s*residual\s*)?(,)?(\s*g1\s*)?(,)?(\s*g2\s*)?(,)?(\s*g3\s*)?\]', 'MaxArgValues' );
-    FileText = regexprep( FileText, 'residual\s*=\s*zeros\(\s*\d+\s*,\s*\d+\s*\)', [ 'MaxArgValues = zeros( ' StringNumberOfMax ', 2 )' ] );
+    FileText = regexprep( FileText, 'residual\s*=\s*zeros\(\s*\d+\s*,\s*\d+\s*\)', [ 'MaxArgValues = zeros( ' int2str( NumberOfMax ) ', 2 )' ] );
     FileText = regexprep( FileText, 'dynareOBCMaxArgA(\d+)__', 'MaxArgValues\($1,1\)' );
     FileText = regexprep( FileText, 'dynareOBCMaxArgB(\d+)__', 'MaxArgValues\($1,2\)' );
     if NumberOfMax > 0
-        FileText = regexprep( FileText, [ '(?<=(dynareOBCMaxFunc' StringNumberOfMax '__[^;]*;)).*$' ], '' );
+        FileText = regexprep( FileText, '(\<dynareOBCMaxFunc\d+__\s+=\s+[^;]*;)(?!.*(\<dynareOBCMaxFunc\d+__\s+=\s+[^;]*;)).*$', '' );
     else
         FileText = regexprep( FileText, '(?<=(MaxArgValues = zeros[^;]*;)).*$', '' );
     end
