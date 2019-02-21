1cc4399ed230b2752d4538134e5f3f16318dfc2f
Tom Holden <thomas.holden@gmail.com>
2019-02-21 15:33:52 +0100

    new set of plots

--- Examples/SmetsWouters2003/GenerateTemporaryPLTPlots.m
+++ Examples/SmetsWouters2003/GenerateTemporaryPLTPlots.m
@@ -1,2 +1,6 @@
-dynareOBC SW03IRFp.mod ShockScale=22.5 SkipFirstSolutions=1
-dynareOBC SW03IRFpTPLT.mod ShockScale=22.5 SkipFirstSolutions=1
+close all 
+dynareOBC SW03IRFp.mod ShockScale=22.5 DisplayBoundsSolutionProgress MultiThreadBoundsProblem TimeToEscapeBounds=32 SkipFirstSolutions=1
+dynareOBC SW03IRFp.mod ShockScale=22.5 DisplayBoundsSolutionProgress MultiThreadBoundsProblem TimeToEscapeBounds=32 SkipFirstSolutions=1 IRFsForceAtBoundIndices=[5]
+dynareOBC SW03IRFp.mod ShockScale=22.5 DisplayBoundsSolutionProgress MultiThreadBoundsProblem TimeToEscapeBounds=32 SkipFirstSolutions=1 IRFsForceAtBoundIndices=[6]
+dynareOBC SW03IRFp.mod ShockScale=22.5 DisplayBoundsSolutionProgress MultiThreadBoundsProblem TimeToEscapeBounds=32 SkipFirstSolutions=1 IRFsForceAtBoundIndices=[1] IRFsForceNotAtBoundIndices=[4]
+dynareOBC SW03IRFp.mod ShockScale=22.5 DisplayBoundsSolutionProgress MultiThreadBoundsProblem TimeToEscapeBounds=32 SkipFirstSolutions=1 IRFsForceAtBoundIndices=[1] IRFsForceNotAtBoundIndices=[3:4]
