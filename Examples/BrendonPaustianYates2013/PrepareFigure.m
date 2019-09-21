d10b1ad644c9a5b7278615c7e854e6c1ec19e10b
Tom Holden <thomas.holden@gmail.com>
2019-09-21 10:35:49 +0200

    figure prettification

---
+++ Examples/BrendonPaustianYates2013/PrepareFigure.m
@@ -0,0 +1,8 @@
+Titles = { '$x_{y,t}$', '$x_{\pi,t}$', '$x_{i,t}$' };
+
+for i = 1 : 3
+    subplot( 3, 1, i );
+    set( gca, 'FontName', 'TeXGyrePagella' );
+    set( gca, 'FontSize', 40 );
+    title( Titles{ i }, 'Interpreter','latex' );
+end
