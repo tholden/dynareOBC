622a6473bf85acdc483518f3024a8a28508df105
Tom Holden <thomas.holden@gmail.com>
2019-09-21 18:38:35 +0200

    space

--- Examples/BrendonPaustianYates2013/PrepareFigure.m
+++ Examples/BrendonPaustianYates2013/PrepareFigure.m
@@ -4,5 +4,5 @@ for i = 1 : 3
     subplot( 3, 1, i );
     set( gca, 'FontName', 'TeXGyrePagella' );
     set( gca, 'FontSize', 40 );
-    title( Titles{ i }, 'Interpreter','latex' );
+    title( Titles{ i }, 'Interpreter', 'latex' );
 end
